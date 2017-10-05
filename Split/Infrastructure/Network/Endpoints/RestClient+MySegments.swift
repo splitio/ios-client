3f2cd23aa0bfb131d58b8eb30bea9aae7c48e06a
Brian Sztamfater <bsztamfater@makingsense.com>
2017-10-05 01:10:15 -0300

    Add Unit Tests and fixes on MySegments response handler

--- Split/Infrastructure/Network/Endpoints/RestClient+MySegments.swift
+++ Split/Infrastructure/Network/Endpoints/RestClient+MySegments.swift
@@ -13,9 +13,11 @@ extension RestClient {
     
     func getMySegments(user: String, completion: @escaping (DataResult<[String]>) -> Void) {
         self.execute(target: StagingTarget.GetMySegments(user: user), completion: completion) { json in
-            return json["mySegments"].arrayValue.map { (json: JSON) -> String in
-                return json["name"].stringValue
-            }
+            return json["mySegments"].arrayValue
+                .filter{ $0["name"] != JSON.null }
+                .map { (json: JSON) -> String in
+                    return json["name"].stringValue
+                }
         }
     }
     
