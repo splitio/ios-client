bd2083a3b6e57e12666c1dad0a986a76c8619fee
Javier <javrudsky@gmail.com>
2018-07-06 15:14:23 -0300

    Version 1.2.0

--- Split/Version.swift
+++ Split/Version.swift
@@ -9,7 +9,7 @@ import Foundation
 
 class Version {
     private static let name:String = "ios"
-    private static let number:String = "1.1.0"
+    private static let number:String = "1.2.0"
 
     public static func toString() -> String {
         return name + "-" + number
