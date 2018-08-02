b2a49b1169889eb7e5b8d4e225cf569d973a9f2f
Javier L. Avrudsky <javier.avrudsky@black-tobacco.com>
2018-08-02 18:24:23 -0300

    Added in memory cache

--- Split/Fetcher Engine/Cache/InMemorySplitCache.swift
+++ Split/Fetcher Engine/Cache/InMemorySplitCache.swift
@@ -9,44 +9,62 @@
 import Foundation
 
 @objc public final class InMemorySplitCache: NSObject, SplitCacheProtocol {
-    
+    private let queueName = "split.inmemcache-queue"
+    private var queue: DispatchQueue
     private var splits: [String: Split]
     private var changeNumber: Int64
     
     public init(splits: [String: Split] = [:], changeNumber: Int64 = -1) {
+        self.queue = DispatchQueue(label: queueName, attributes: .concurrent)
         self.splits = splits
         self.changeNumber = changeNumber
     }
     
     public func addSplit(splitName: String, split: Split) -> Bool {
-        self.splits[splitName] = split
+        queue.async(flags: .barrier) {
+            self.splits[splitName] = split
+        }
         return true
     }
     
     public func removeSplit(splitName: String) -> Bool {
-        self.splits.removeValue(forKey: splitName)
+        queue.async(flags: .barrier) {
+            self.splits.removeValue(forKey: splitName)
+        }
         return true
     }
     
     public func setChangeNumber(_ changeNumber: Int64) -> Bool {
-        self.changeNumber = changeNumber
+        queue.async(flags: .barrier) {
+            self.changeNumber = changeNumber
+        }
         return true
     }
     
     public func getChangeNumber() -> Int64 {
-        return self.changeNumber
+        var number: Int64 = -1
+        queue.sync {
+            number = self.changeNumber
+        }
+        return number
     }
     
     public func getSplit(splitName: String) -> Split? {
-        return self.splits[splitName]
+        var split: Split? = nil
+        queue.sync {
+            split = self.splits[splitName]
+        }
+        return split
     }
     
     public func getAllSplits() -> [Split] {
-        return Array(self.splits.values)
+        var splits = [Split]()
+        queue.sync {
+            splits = Array(self.splits.values)
+        }
+        return splits
     }
     
     public func clear() {
-        return self.splits.removeAll()
     }
-    
 }
