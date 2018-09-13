863a286be85d34fba6edfe632fdaf2a248661a7a
Javier L. Avrudsky <javier.avrudsky@black-tobacco.com>
2018-09-13 11:49:01 -0300

    Version 3.1.2

--- Split/Version.swift
+++ Split/Version.swift
@@ -9,7 +9,7 @@ import Foundation
 
 class Version {
     private static let name:String = "ios"
-    private static let number:String = "1.3.1"
+    private static let number:String = "1.3.2"
 
     public static func toString() -> String {
         return name + "-" + number
