9d378e964a88e41df0df540f56c3342052027642
Javier L. Avrudsky <javier.avrudsky@black-tobacco.com>
2018-09-28 16:51:06 -0300

    Added metrics models

---
+++ Split/Metrics/Models/MetricCounter.swift
@@ -0,0 +1,14 @@
+//
+//  MetricCounter.swift
+//  Split
+//
+//  Created by Javier on 28/09/2018.
+//  Copyright © 2018 Split. All rights reserved.
+//
+
+import Foundation
+
+struct MetricCounter: Codable {
+    var name: String
+    var delta: Int64
+}
