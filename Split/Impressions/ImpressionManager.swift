4cad9ab4eb79db7f282ac78a16e3ea7c6bd278c3
Javier L. Avrudsky <javier.avrudsky@black-tobacco.com>
2018-10-03 14:19:31 -0300

    Migration to Swift 4.2

--- Split/Impressions/ImpressionManager.swift
+++ Split/Impressions/ImpressionManager.swift
@@ -41,8 +41,8 @@ class ImpressionManager {
     }
     
     deinit {
-        NotificationCenter.default.removeObserver(self, name: .UIApplicationDidEnterBackground, object: nil)
-        NotificationCenter.default.removeObserver(self, name: .UIApplicationDidBecomeActive, object: nil)
+        NotificationCenter.default.removeObserver(self, name: UIApplication.didEnterBackgroundNotification, object: nil)
+        NotificationCenter.default.removeObserver(self, name: UIApplication.didBecomeActiveNotification, object: nil)
     }
 }
 
@@ -181,7 +181,7 @@ extension ImpressionManager {
     }
     
     func subscribeNotifications() {
-        NotificationCenter.default.addObserver(self, selector: #selector(applicationDidEnterBackground), name: .UIApplicationDidEnterBackground, object: nil)
-        NotificationCenter.default.addObserver(self, selector: #selector(applicationDidBecomeActive), name: .UIApplicationDidBecomeActive, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(applicationDidEnterBackground), name: UIApplication.didEnterBackgroundNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(applicationDidBecomeActive), name: UIApplication.didBecomeActiveNotification, object: nil)
     }
 }
