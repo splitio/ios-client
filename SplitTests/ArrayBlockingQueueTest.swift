63723376b9ab75c7a24583a474100a63f2ecaa77
Javier L. Avrudsky <javier.avrudsky@black-tobacco.com>
2018-10-11 14:46:54 -0300

    Removed Quick and Nimble from InMemorySegments tests

--- SplitTests/ArrayBlockingQueueTest.swift
+++ SplitTests/ArrayBlockingQueueTest.swift
@@ -14,11 +14,9 @@ import XCTest
 class ArrayBlockingQueueTests: XCTestCase {
     
     override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
     }
     
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
     
     func testArrayBlockingQueue() {
