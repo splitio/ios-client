e89749fa5e2bbb51ac576b5aeb2a30d2abe0ed3d
Javier L. Avrudsky <javier.avrudsky@black-tobacco.com>
2018-10-15 18:14:32 -0300

    Quick and nimble removed from Manager tests

--- SplitTests/SplitManagerTest.swift
+++ SplitTests/SplitManagerTest.swift
@@ -7,136 +7,99 @@
 //
 
 import Foundation
-import Quick
-import Nimble
+import XCTest
 
 @testable import Split
 
-class SplitManagerTest: QuickSpec {
+class SplitManagerTest: XCTestCase {
     
-    override func spec() {
-        
-        describe("SplitManagerTest") {
-            let bundle = Bundle(for: type(of: self))
-            let path = bundle.path(forResource: "splits", ofType: "json")!
-            let json = try? Data(contentsOf: URL(fileURLWithPath: path)).stringRepresentation
-            let loadedSplits = try? JSON.encodeFrom(json: json!, to: [Split].self)
-            context("Initial split loaded") {
-                let cache: SplitCacheProtocol = SplitCacheStub(splits: loadedSplits!, changeNumber:1)
-                let fetcher: SplitFetcher = LocalSplitFetcher(splitCache: cache)
-                let manager: SplitManagerProtocol = SplitManager(splitFetcher: fetcher)
-                let splits = manager.splitNames
-                let names = manager.splitNames
-                
-                it("split count") {
-                    expect(splits.count).to(equal(6))
-                }
-                
-                it("Names are ok"){
-                   expect(names.sorted().joined(separator: ",").lowercased()).to(equal("sample_feature0,sample_feature1,sample_feature2,sample_feature3,sample_feature4,sample_feature5"))
-                }
-
-                describe("Search By Name"){
-                    context("Name Lowercase"){
-                        let split = manager.split(featureName: "sample_feature0")
-                        it("Found"){
-                            expect(split).toNot(beNil())
-                            expect(split?.name?.lowercased()).to(equal("sample_feature0"))
-                        }
-                    }
-                    
-                    context("Name Uppercase"){
-                        let split = manager.split(featureName: "SAMPLE_FEATURE0")
-                        it("Found"){
-                            expect(split).toNot(beNil())
-                            expect(split?.name?.lowercased()).to(equal("sample_feature0"))
-                        }
-                    }
-                    
-                    context("Non Existing"){
-                        let split = manager.split(featureName: "SAMPLE_FEATURE99")
-                        it("Not Found"){
-                            expect(split).to(beNil())
-                        }
-                    }
-                }
-                
-                describe("Check Split"){
-                    context("Feature 0"){
-                        let split = manager.split(featureName: "sample_feature0")!
-                        let treatments = split.treatments!
-                        
-                        it("Basic Info"){
-                            expect(split.name!.lowercased()).to(equal("sample_feature0"))
-                            expect(split.changeNumber).to(equal(1))
-                            expect(split.killed).to(beFalse())
-                            expect(split.trafficType).to(equal("custom"))
-                        }
-                        
-                        it("Treatments"){
-                            expect(treatments.count).to(equal(6))
-                             expect(treatments.sorted().joined(separator: ",").lowercased()).to(equal("t1_0,t2_0,t3_0,t4_0,t5_0,t6_0"))
-                        }
-                    }
-                    context("Feature 1"){
-                        let split = manager.split(featureName: "sample_feature1")!
-                        let treatments = split.treatments!
-                        
-                        it("Basic Info"){
-                            expect(split.name!.lowercased()).to(equal("sample_feature1"))
-                            expect(split.changeNumber).to(equal(1))
-                            expect(split.killed).toNot(beFalse())
-                            expect(split.trafficType).to(equal("custom1"))
-                        }
-                        
-                        it("Treatments"){
-                            expect(treatments.count).to(equal(6))
-                            expect(treatments.sorted().joined(separator: ",").lowercased()).to(equal("t1_1,t2_1,t3_1,t4_1,t5_1,t6_1"))
-                        }
-                    }
-                }
-            }
-            
-            context("Added One Split") {
-                let cache: SplitCacheProtocol = SplitCacheStub(splits: loadedSplits!, changeNumber: 1)
-                let fetcher: SplitFetcher = LocalSplitFetcher(splitCache: cache)
-                let manager: SplitManagerProtocol = SplitManager(splitFetcher: fetcher)
-                let path = bundle.path(forResource: "split_sample_feature6", ofType: "json")!
-                let newSplit = try! JSON(Data(contentsOf: URL(fileURLWithPath: path))).decode(Split.self)!
-                _ = cache.addSplit(splitName: newSplit.name!, split: newSplit)
-                _ = cache.setChangeNumber(2)
-                let splits = manager.splits
-                let names = manager.splitNames
-                it("split count") {
-                    expect(splits.count).to(equal(7))
-                }
-                
-                it("Names are ok"){
-                    expect(names.sorted().joined(separator: ",").lowercased()).to(equal("sample_feature0,sample_feature1,sample_feature2,sample_feature3,sample_feature4,sample_feature5,sample_feature6"))
-                }
-            }
-            
-            context("Removed One Split") {
-                let cache: SplitCacheProtocol = SplitCacheStub(splits: loadedSplits!, changeNumber: 1)
-                let fetcher: SplitFetcher = LocalSplitFetcher(splitCache: cache)
-                let manager: SplitManagerProtocol = SplitManager(splitFetcher: fetcher)
-                _ = cache.removeSplit(splitName: "sample_feature4")
-                _ = cache.setChangeNumber(2)
-                let splits = manager.splits
-                let names = manager.splitNames
-                it("split count") {
-                    expect(splits.count).to(equal(5))
-                }
-                
-                it("Names are ok"){
-                    expect(names.sorted().joined(separator: ",").lowercased()).to(equal("sample_feature0,sample_feature1,sample_feature2,sample_feature3,sample_feature5"))
-                }
-            }
-            
-        }
-        
+    var loadedSplits: [Split]!
+    var manager: SplitManagerProtocol!
+    var cache: SplitCacheProtocol!
+    
+    override func setUp() {
+        let bundle = Bundle(for: type(of: self))
+        let path = bundle.path(forResource: "splits", ofType: "json")!
+        let json = try? Data(contentsOf: URL(fileURLWithPath: path)).stringRepresentation
+        loadedSplits = try? JSON.encodeFrom(json: json!, to: [Split].self)
+        cache = SplitCacheStub(splits: loadedSplits!, changeNumber:1)
+        let fetcher: SplitFetcher = LocalSplitFetcher(splitCache: cache)
+        manager = SplitManager(splitFetcher: fetcher)
     }
     
+    override func tearDown() {
+    }
     
+    func testInitialSplitLoaded() {
+        
+        let splits = manager.splitNames
+        let names = manager.splitNames
+        
+        var expectedSplitNames = [String]()
+        for i in 0...5 {
+            expectedSplitNames.append("sample_feature\(i)")
+        }
+        XCTAssertEqual(splits.count, 6, "Split count should be 6")
+        XCTAssertEqual(names.sorted().joined(separator: ",").lowercased(), expectedSplitNames.joined(separator: ","), "Loaded splits names are not correct")
+        
+        let splitLowercase = manager.split(featureName: "sample_feature0")
+        XCTAssertNotNil(splitLowercase, "Lowercase split should be found")
+        XCTAssertEqual(splitLowercase?.name?.lowercased(), "sample_feature0", "Lowercase split name is not equal to expected")
+        
+        let splitUppercase = manager.split(featureName: "SAMPLE_FEATURE0")
+        XCTAssertNotNil(splitUppercase, "Uppercase split should be found")
+        XCTAssertEqual(splitUppercase?.name?.lowercased(), "sample_feature0", "Uppercase split name is not equal to expected")
+        
+        let splitNotExistent = manager.split(featureName: "SAMPLE_FEATURE99")
+        XCTAssertNil(splitNotExistent, "Non existent split should be nil")
+    }
     
+    func testSplitInfo(){
+        
+        let split0 = manager.split(featureName: "sample_feature0")!
+        let treatments0 = split0.treatments!
+        
+        XCTAssertEqual(split0.name!.lowercased(), "sample_feature0", "Split0 name")
+        XCTAssertEqual(split0.changeNumber, 1, "Split0 change number")
+        XCTAssertFalse(split0.killed!, "Split0 killed")
+        XCTAssertEqual(split0.trafficType, "custom", "Split0 traffic type")
+        
+        XCTAssertEqual(treatments0.count, 6, "Split0 treatment count")
+        XCTAssertEqual(treatments0.sorted().joined(separator: ",").lowercased(), "t1_0,t2_0,t3_0,t4_0,t5_0,t6_0", "Split0 treatment names")
+        
+        let split1 = manager.split(featureName: "sample_feature1")!
+        let treatments1 = split1.treatments!
+        
+        XCTAssertEqual(split1.name!.lowercased(), "sample_feature1", "Split1 name")
+        XCTAssertEqual(split1.changeNumber, 1, "Split1 change number")
+        XCTAssertTrue(split1.killed!, "Split1 killed")
+        XCTAssertEqual(split1.trafficType, "custom1", "Split1 traffic type")
+        XCTAssertEqual(treatments1.count, 6, "Split1 treatment count")
+        XCTAssertEqual(treatments1.sorted().joined(separator: ",").lowercased(), "t1_1,t2_1,t3_1,t4_1,t5_1,t6_1", "Split1 treatment names")
+    }
+    
+    func testAddOneSplit() {
+        let bundle = Bundle(for: type(of: self))
+        let path = bundle.path(forResource: "split_sample_feature6", ofType: "json")!
+        let newSplit = try! JSON(Data(contentsOf: URL(fileURLWithPath: path))).decode(Split.self)!
+        _ = cache.addSplit(splitName: newSplit.name!, split: newSplit)
+        _ = cache.setChangeNumber(2)
+        let splits = manager.splits
+        let names = manager.splitNames
+        XCTAssertEqual(splits.count, 7, "Added one split count")
+        XCTAssertEqual(names.sorted().joined(separator: ",").lowercased(), "sample_feature0,sample_feature1,sample_feature2,sample_feature3,sample_feature4,sample_feature5,sample_feature6", "Added one split name check")
+    }
+    
+    func testRemoveOneSplit() {
+        let cache: SplitCacheProtocol = SplitCacheStub(splits: loadedSplits!, changeNumber: 1)
+        let fetcher: SplitFetcher = LocalSplitFetcher(splitCache: cache)
+        let manager: SplitManagerProtocol = SplitManager(splitFetcher: fetcher)
+        _ = cache.removeSplit(splitName: "sample_feature4")
+        _ = cache.setChangeNumber(2)
+        let splits = manager.splits
+        let names = manager.splitNames
+        
+        XCTAssertEqual(splits.count, 5, "Removed split count")
+        XCTAssertEqual(names.sorted().joined(separator: ",").lowercased(), "sample_feature0,sample_feature1,sample_feature2,sample_feature3,sample_feature5", "Removed split names check")
+    }
 }
