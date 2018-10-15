3b8ab6321aea32503ab97d492a50f97e6a513072
Javier L. Avrudsky <javier.avrudsky@black-tobacco.com>
2018-10-15 14:31:13 -0300

    1. Removed Quick and Nimble from SplitChangeFetcher tests. 2. SplitChangeFetcher adapted to new SplitChangeFetcher fetch way. 3. Refactored some duplicated code from SplitChangeFetcher

--- Split/FetcherEngine/HttpSplitChangeFetcher.swift
+++ Split/FetcherEngine/HttpSplitChangeFetcher.swift
@@ -16,7 +16,7 @@ public enum FecthingPolicy {
 class HttpSplitChangeFetcher: NSObject, SplitChangeFetcher {
     
     private let restClient: RestClient
-    private let splitChangeCache: SplitChangeCache?
+    private let splitChangeCache: SplitChangeCache
     
     init(restClient: RestClient, splitCache: SplitCacheProtocol) {
         self.restClient = restClient
@@ -25,35 +25,23 @@ class HttpSplitChangeFetcher: NSObject, SplitChangeFetcher {
     
     func fetch(since: Int64, policy: FecthingPolicy) throws -> SplitChange? {
         
-        var reachable: Bool = true
-
-        if policy == .cacheOnly {
-            return self.splitChangeCache?.getChanges(since: -1)
+        if policy == .cacheOnly || !restClient.isSdkServerAvailable() {
+            return self.splitChangeCache.getChanges(since: -1)
         }
         
-        if let reachabilityManager = NetworkReachabilityManager(host: "sdk.split.io/api/version") {
-            if (!reachabilityManager.isReachable)  {
-                reachable = false
-            }
+        let semaphore = DispatchSemaphore(value: 0)
+        var requestResult: DataResult<SplitChange>?
+        restClient.getSplitChanges(since: since) { result in
+            requestResult = result
+            semaphore.signal()
         }
+        semaphore.wait()
         
-        if !reachable {
-            return self.splitChangeCache?.getChanges(since: since)
-        } else {
-            let semaphore = DispatchSemaphore(value: 0)
-            var requestResult: DataResult<SplitChange>?
-            restClient.getSplitChanges(since: since) { result in
-                requestResult = result
-                semaphore.signal()
-            }
-            semaphore.wait()
-            
-            guard let change: SplitChange = try requestResult?.unwrap(), change.isValid else {
-                throw NSError(domain: "Null split changes", code: -1, userInfo: nil)
-            }
-            _ = self.splitChangeCache?.addChange(splitChange: change)
-            return change
+        guard let change: SplitChange = try requestResult?.unwrap(), change.isValid else {
+            throw NSError(domain: "Null split changes", code: -1, userInfo: nil)
         }
+        _ = self.splitChangeCache.addChange(splitChange: change)
+        return change
     }
     
 }
