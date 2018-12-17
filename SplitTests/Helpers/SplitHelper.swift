c832b1112f4ff9eda3ec577146d4a27909d37782
Javier L. Avrudsky <javier.avrudsky@black-tobacco.com>
2018-12-17 12:03:21 -0300

    Fixed traffic allocation 1% issue and test added

---
+++ SplitTests/Helpers/SplitHelper.swift
@@ -0,0 +1,24 @@
+//
+//  SplitHelper.swift
+//  Split
+//
+//  Created by Javier L. Avrudsky on 17/12/2018.
+//  Copyright © 2018 Split. All rights reserved.
+//
+
+import Foundation
+@testable import Split
+
+class SplitHelper {
+    
+    func loadSplitFromFile(name: String) -> Split? {
+        var split: Split?
+        do {
+            split = try JSON.encodeFrom(json: FileHelper.readDataFromFile(sourceClass: self, name: name, type: "json")!, to: Split.self)
+        } catch {
+            print("Error loading split from file \(name)")
+        }
+        return split
+    }
+    
+}
