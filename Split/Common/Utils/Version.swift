9c77eb514108f385ebe6a4ab51a3ebaf981f7443
Javier L. Avrudsky <javier.avrudsky@black-tobacco.com>
2018-12-18 16:12:23 -0300

    Version 1.6.0-sdks-341.1

--- Split/Common/Utils/Version.swift
+++ Split/Common/Utils/Version.swift
@@ -9,7 +9,7 @@ import Foundation
 
 class Version {
     private static let name:String = "ios"
-    private static let number:String = "1.5.8-skds-341.1"
+    private static let number:String = "1.6.0-skds-341.1"
 
     public static func toString() -> String {
         return name + "-" + number
