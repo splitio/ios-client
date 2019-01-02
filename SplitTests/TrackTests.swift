eae9b6ec880fe53a0b44d2a451d6e4d3aba4ed73
Javier L. Avrudsky <javier.avrudsky@black-tobacco.com>
2019-01-02 15:26:11 -0300

    temp

---
+++ SplitTests/TrackTests.swift
@@ -0,0 +1,26 @@
+//
+//  TrackTests.swift
+//  SplitTests
+//
+//  Created by Javier L. Avrudsky on 02/01/2019.
+//  Copyright © 2019 Split. All rights reserved.
+//
+
+import XCTest
+@testable import Split
+
+class TrackTests: XCTestCase {
+
+    override func setUp() {
+        
+    }
+
+    override func tearDown() {
+        
+    }
+
+    func testTrack() {
+        
+    }
+
+}
