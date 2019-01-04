964ef9481f57a2e9594acf19de1d9e87bcbd0f46
Javier L. Avrudsky <javier.avrudsky@black-tobacco.com>
2019-01-04 16:19:04 -0300

    Added isEmpty function to String extension

---
+++ Split/Common/Extensions/String+Utils.swift
@@ -0,0 +1,15 @@
+//
+//  String+Utils.swift
+//  Split
+//
+//  Created by Javier L. Avrudsky on 04/01/2019.
+//  Copyright © 2019 Split. All rights reserved.
+//
+
+import Foundation
+
+extension String {
+    public func isEmpty() -> Bool {
+        return (self.trimmingCharacters(in: .whitespacesAndNewlines) == "")
+    }
+}
