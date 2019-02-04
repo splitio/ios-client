e7d498d84a653e2e2a4ad45ffd55cf84d46324cc
Javier L. Avrudsky <javier.avrudsky@black-tobacco.com>
2019-02-04 08:53:40 -0300

    TMP COMMIT

---
+++ Split/Localhost/LocalhostSplitsParser.swift
@@ -0,0 +1,21 @@
+//
+//  LocalhostSplitsParser.swift
+//  Split
+//
+//  Created by Javier L. Avrudsky on 31/01/2019.
+//  Copyright © 2019 Split. All rights reserved.
+//
+
+import Foundation
+/**
+ Data structure to hold parsed splits and its corresponding treatment
+ */
+typealias LocalhostSplits = [[String: String]]
+
+/**
+ Interface to implement by classes intended to parse
+ splits loaded for localhost feature
+ */
+protocol LocalhostSplitsParser {
+    func parseContent(_ content: String) -> LocalhostSplits
+}
---
+++ Split/Localhost/SpaceDelimitedLocalhostSplitsParser.swift
@@ -0,0 +1,15 @@
+//
+//  SpaceSeparatedLocalhostSplitsParser.swift
+//  Split
+//
+//  Created by Javier L. Avrudsky on 31/01/2019.
+//  Copyright © 2019 Split. All rights reserved.
+//
+
+import Foundation
+
+class SpaceDelimitedLocalhostSplitsParser: LocalhostSplitsParser {
+    func parseContent(_ content: String) -> LocalhostSplits {
+        return [[String:String]]()
+    }
+}
