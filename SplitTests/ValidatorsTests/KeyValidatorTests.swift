0fa28f6ad95c008cfd4286f94e9961f94daab357
Javier L. Avrudsky <javier.avrudsky@black-tobacco.com>
2019-05-01 11:13:33 -0300

    Some key validator tests improvements

--- SplitTests/ValidatorsTests/KeyValidatorTests.swift
+++ SplitTests/ValidatorsTests/KeyValidatorTests.swift
@@ -31,16 +31,16 @@ class KeyValidatorTests: XCTestCase {
     func testNullMatchingKey() {
         let errorInfo = validator.validate(matchingKey: nil, bucketingKey: nil);
         XCTAssertNotNil(errorInfo)
-        XCTAssertNotNil(errorInfo?.error)
-        XCTAssertNotNil(errorInfo?.errorMessage)
+        XCTAssertTrue(errorInfo?.isError ?? false)
+        XCTAssertEqual("you passed a null key, the key must be a non-empty string", errorInfo?.errorMessage)
         XCTAssertEqual(errorInfo?.warnings.count, 0)
     }
     
     func testInvalidEmptyMatchingKey() {
         let errorInfo = validator.validate(matchingKey: "", bucketingKey: nil);
         XCTAssertNotNil(errorInfo)
-        XCTAssertNotNil(errorInfo?.error)
-        XCTAssertNotNil(errorInfo?.errorMessage)
+        XCTAssertTrue(errorInfo?.isError ?? false)
+        XCTAssertEqual("you passed an empty string, matching key must a non-empty string", errorInfo?.errorMessage)
         XCTAssertEqual(errorInfo?.warnings.count, 0)
     }
     
@@ -48,16 +48,16 @@ class KeyValidatorTests: XCTestCase {
         let key = String(repeating: "p", count: 256)
         let errorInfo = validator.validate(matchingKey: key, bucketingKey: nil);
         XCTAssertNotNil(errorInfo)
-        XCTAssertNotNil(errorInfo?.error)
-        XCTAssertNotNil(errorInfo?.errorMessage)
+        XCTAssertTrue(errorInfo?.isError ?? false)
+        XCTAssertEqual("matching key too long - must be \(ValidationConfig.default.maximumKeyLength) characters or less", errorInfo?.errorMessage)
         XCTAssertEqual(errorInfo?.warnings.count, 0)
     }
     
     func testInvalidEmptyBucketingKey() {
         let errorInfo = validator.validate(matchingKey: "key1", bucketingKey: "");
         XCTAssertNotNil(errorInfo)
-        XCTAssertNotNil(errorInfo?.error)
-        XCTAssertNotNil(errorInfo?.errorMessage)
+        XCTAssertTrue(errorInfo?.isError ?? false)
+        XCTAssertEqual("you passed an empty string, bucketing key must be null or a non-empty string", errorInfo?.errorMessage)
         XCTAssertEqual(errorInfo?.warnings.count, 0)
     }
     
@@ -65,8 +65,8 @@ class KeyValidatorTests: XCTestCase {
         let bkey = String(repeating: "p", count: 256)
         let errorInfo = validator.validate(matchingKey: "key1", bucketingKey: bkey);
         XCTAssertNotNil(errorInfo)
-        XCTAssertNotNil(errorInfo?.error)
-        XCTAssertNotNil(errorInfo?.errorMessage)
+        XCTAssertTrue(errorInfo?.isError ?? false)
+        XCTAssertEqual("bucketing key too long - must be \(ValidationConfig.default.maximumKeyLength) characters or less", errorInfo?.errorMessage)
         XCTAssertEqual(errorInfo?.warnings.count, 0)
     }
 }
