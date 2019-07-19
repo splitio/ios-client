95f2397b8594af2b8081c2d2b61948990e2a07bd
Javier L. Avrudsky <javier.avrudsky@black-tobacco.com>
2019-07-19 14:48:12 -0300

    Basic integration tests setup

--- SplitTests/Localhost/SplitIntegrationTest.swift
+++ SplitTests/Localhost/SplitIntegrationTest.swift
@@ -11,11 +11,95 @@ import XCTest
 
 class SplitIntegrationTests: XCTestCase {
     
+    let kNeverRefreshRate = 9999999
+    var webServer: MockWebServer!
+    var splitChange: SplitChange?
+    
     override func setUp() {
+        if splitChange == nil {
+            splitChange = loadSplitsChangeFile()
+        }
+        setupServer()
     }
     
     override func tearDown() {
+        stopServer()
     }
+    
+    private func setupServer() {
+        webServer = MockWebServer()
+        webServer.routeGet(path: "/mySegments/:user_id", data: "{\"mySegments\":[{ \"id\":\"id1\", \"name\":\"segment1\"}, { \"id\":\"id1\", \"name\":\"segment2\"}]}")
+        webServer.routeGet(path: "/splitChanges?since=:param", data: try? Json.encodeToJson(splitChange))
+        webServer.routePost(path: "/events/bulk", data: nil)
+        webServer.start()
+    }
+    
+    private func stopServer() {
+        webServer.stop()
+    }
+    
+    func testControlTreatment() {
+        let apiKey = "99049fd8653247c5ea42bc3c1ae2c6a42bc3"
+        let dataFolderName = "2a1099049fd8653247c5ea42bOIajMRhH0R0FcBwJZM4ca7zj6HAq1ZDS"
+        let matchingKey = "CUSTOMER_ID"
+        let trafficType = "account"
+        
+        let splitConfig: SplitClientConfig = SplitClientConfig()
+        splitConfig.featuresRefreshRate = 30
+        splitConfig.segmentsRefreshRate = 30
+        splitConfig.impressionRefreshRate = 30
+        splitConfig.sdkReadyTimeOut = 60000
+        splitConfig.trafficType = trafficType
+        splitConfig.eventsPerPush = 10
+        splitConfig.eventsQueueSize = 100
+        splitConfig.targetSdkEndPoint = "http://localhost:8080"
+        splitConfig.targetEventsEndPoint = "http://localhost:8080"
+        
+        let key: Key = Key(matchingKey: matchingKey, bucketingKey: nil)
+        let builder = DefaultSplitFactoryBuilder()
+        let factory = builder.setApiKey(apiKey).setKey(key).setConfig(splitConfig).build()
+        let client = factory?.client
+        let sdkReadyExpectation = XCTestExpectation(description: "SDK READY Expectation")
+        var timeOutFired = false
+        var sdkReadyFired = false
+        
+        client?.on(event: SplitEvent.sdkReady) {
+            sdkReadyFired = true
+            sdkReadyExpectation.fulfill()
+        }
+        
+        client?.on(event: SplitEvent.sdkReadyTimedOut) {
+            timeOutFired = true
+            sdkReadyExpectation.fulfill()
+        }
+        
+        wait(for: [sdkReadyExpectation], timeout: 400000.0)
+        
+        let t1 = client?.getTreatment("FACUNDO_TEST")
+        let t2 = client?.getTreatment("NO_EXISTING_FEATURE")
+        let treatmentConfigEmojis = client?.getTreatmentWithConfig("Welcome_Page_UI")
+        
+        XCTAssertTrue(sdkReadyFired)
+        XCTAssertFalse(timeOutFired)
+        XCTAssertEqual("off", t1)
+        XCTAssertEqual(SplitConstants.CONTROL, t2)
+        XCTAssertEqual("{\"the_emojis\":\"\\uD83D\\uDE01 -- áéíóúöÖüÜÏëç\"}", treatmentConfigEmojis?.config)
+        
+    }
+    
+    private func loadSplitsChangeFile() -> SplitChange? {
+        return loadSplitChangeFile(name: "splitchanges_1")
+    }
+    
+    private func loadSplitChangeFile(name fileName: String) -> SplitChange? {
+        if let file = FileHelper.readDataFromFile(sourceClass: self, name: fileName, type: "json"),
+            let change = try? Json.encodeFrom(json: file, to: SplitChange.self),
+            let splits = change.splits {
+            return change
+        }
+        return nil
+    }
+    
     
 }
 
