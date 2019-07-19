1fb9da3a46605974c2937c242297bcf68539e4c8
Javier L. Avrudsky <javier.avrudsky@black-tobacco.com>
2019-07-19 10:59:01 -0300

    Adding Swifter library to mock webserver. Added Mocked web server component

---
+++ SplitTests/Helpers/MockWebServer.swift
@@ -0,0 +1,60 @@
+//
+//  MockWebServer.swift
+//  SplitTests
+//
+//  Created by Javier L. Avrudsky on 18/07/2019.
+//  Copyright © 2019 Split. All rights reserved.
+//
+
+import Foundation
+import Swifter
+
+
+struct ReceivedRequest {
+    var path: String
+    var data: String?
+    var method: String
+}
+
+class MockWebServer {
+    
+    var httpServer: HttpServer
+    var receivedRequests: [ReceivedRequest]
+    
+    init() {
+        httpServer = HttpServer()
+        receivedRequests = [ReceivedRequest]()
+    }
+    
+    func routeGet(path: String, data: String?) {
+        httpServer.GET[path] = { [weak self] request in
+            if let self = self {
+                self.receivedRequests.append(ReceivedRequest(path: request.path, data: self.bytesToString(bytes: request.body), method: "GET"))
+            }
+            return HttpResponse.ok(.text(data ?? ""))
+        }
+    }
+    
+    private func bytesToString(bytes: [UInt8]?) -> String? {
+        guard let bytes = bytes else { return nil }
+        return String(bytes: bytes, encoding: .utf8)
+    }
+    
+    func routePost(path: String, data: String?) {
+        httpServer.POST[path] = { request in
+            HttpResponse.ok(.text(data ?? ""))
+        }
+    }
+    
+    func start() {
+        httpServer.GET["/test)_server"] = { request in
+            return HttpResponse.ok(.text("Test server success!!!"))
+        }
+        try! httpServer.start()
+    }
+    
+    func stop() {
+        httpServer.stop()
+    }
+    
+}
---
+++ SplitTests/Localhost/SplitIntegrationTest.swift
@@ -0,0 +1,21 @@
+//
+//  SplitIntegrationTests.swift
+//  SplitIntegrationTests
+//
+//  Created by Javier L. Avrudsky on 28/03/2019.
+//  Copyright © 2019 Split. All rights reserved.
+//
+
+import XCTest
+@testable import Split
+
+class SplitIntegrationTests: XCTestCase {
+    
+    override func setUp() {
+    }
+    
+    override func tearDown() {
+    }
+    
+}
+
