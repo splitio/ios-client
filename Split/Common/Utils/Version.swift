997ce0a7d92800df5505310bdbc7f6032ff83813
NicoZelaya <zelayadn@gmail.com>
2019-09-10 18:23:09 -0300

    Going back to RC version to test last changes on podspec

--- Split/Common/Utils/Version.swift
+++ Split/Common/Utils/Version.swift
@@ -9,7 +9,7 @@ import Foundation
 
 class Version {
     private static let name: String = "ios"
-    private static let number: String = "2.4.0"
+    private static let number: String = "2.4.0-rc3"
 
     public static func toString() -> String {
         return name + "-" + number
