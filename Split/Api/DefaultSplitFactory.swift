523e3ca944cbd2c6efde6a6535917e17f1141f56
Javier L. Avrudsky <javier.avrudsky@black-tobacco.com>
2019-10-23 20:24:10 -0300

    Moving inicialization from client to factory. Preventing some possible memory leak in event manager resoucers. Improved tests

--- Split/Api/DefaultSplitFactory.swift
+++ Split/Api/DefaultSplitFactory.swift
@@ -13,7 +13,7 @@ import Foundation
  */
 public class DefaultSplitFactory: NSObject, SplitFactory {
 
-    private let defaultClient: SplitClient!
+    private var defaultClient: SplitClient!
     private let defaultManager: SplitManager
 
     public var client: SplitClient {
@@ -41,18 +41,57 @@ public class DefaultSplitFactory: NSObject, SplitFactory {
         let manager = DefaultSplitManager(splitCache: splitCache)
         defaultManager = manager
 
-        defaultClient = DefaultSplitClient( config: config,
-                                            key: key,
-                                            splitCache: splitCache,
-                                            fileStorage: fileStorage,
-                                            destroyHandler: {
-                                                manager.destroy()
+        let mySegmentsCache = MySegmentsCache(matchingKey: key.matchingKey, fileStorage: fileStorage)
+
+        let eventsManager = DefaultSplitEventsManager(config: config)
+        eventsManager.start()
+
+        let httpSplitFetcher = HttpSplitChangeFetcher(restClient: RestClient(), splitCache: splitCache)
+
+        let refreshableSplitFetcher = DefaultRefreshableSplitFetcher(
+            splitChangeFetcher: httpSplitFetcher, splitCache: splitCache, interval: config.featuresRefreshRate,
+            eventsManager: eventsManager)
+
+        let mySegmentsFetcher = HttpMySegmentsFetcher(restClient: RestClient(), mySegmentsCache: mySegmentsCache)
+        let refreshableMySegmentsFetcher = DefaultRefreshableMySegmentsFetcher(
+            matchingKey: key.matchingKey, mySegmentsChangeFetcher: mySegmentsFetcher, mySegmentsCache: mySegmentsCache,
+            interval: config.segmentsRefreshRate, eventsManager: eventsManager)
+
+        super.init()
+
+        let trackConfig = buildTrackConfig(from: config)
+        let trackManager = DefaultTrackManager(config: trackConfig, fileStorage: fileStorage)
+
+        let impressionsConfig = buildImpressionsConfig(from: config)
+        let impressionsManager = DefaultImpressionsManager(config: impressionsConfig, fileStorage: fileStorage)
+
+        defaultClient = DefaultSplitClient(
+            config: config, key: key, splitCache: splitCache, eventsManager: eventsManager, trackManager: trackManager,
+            impressionsManager: impressionsManager, refreshableSplitFetcher: refreshableSplitFetcher,
+            refreshableMySegmentsFetcher: refreshableMySegmentsFetcher, destroyHandler: {
+                refreshableMySegmentsFetcher.stop()
+                refreshableSplitFetcher.stop()
+                impressionsManager.stop()
+                trackManager.stop()
+                manager.destroy()
         })
+
+        eventsManager.getExecutorResources().setClient(client: defaultClient)
+        refreshableSplitFetcher.start()
+        refreshableMySegmentsFetcher.start()
+        trackManager.start()
+        impressionsManager.start()
     }
 
-    private func destroy() {
-        if let manager = defaultManager as? Destroyable {
-            manager.destroy()
-        }
+    private func buildTrackConfig(from splitConfig: SplitClientConfig) -> TrackManagerConfig {
+        return TrackManagerConfig(
+            firstPushWindow: splitConfig.eventsFirstPushWindow, pushRate: splitConfig.eventsPushRate,
+            queueSize: splitConfig.eventsQueueSize, eventsPerPush: splitConfig.eventsPerPush,
+            maxHitsSizeInBytes: splitConfig.maxEventsQueueMemorySizeInBytes)
+    }
+
+    private func buildImpressionsConfig(from splitConfig: SplitClientConfig) -> ImpressionManagerConfig {
+        return ImpressionManagerConfig(pushRate: splitConfig.impressionRefreshRate,
+                                       impressionsPerPush: splitConfig.impressionsChunkSize)
     }
 }
