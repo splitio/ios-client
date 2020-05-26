f11e3deb698e06b32ee0fbdfa2d9fe38d660f63c
Javier L. Avrudsky <javier.avrudsky@black-tobacco.com>
2020-05-26 12:55:38 -0300

    Some sse client tests

--- SplitTests/Network/SSEClientTest.swift
+++ SplitTests/Network/SSEClientTest.swift
@@ -18,8 +18,37 @@ class SSEClientTest: XCTestCase {
     }
 
     func test() {
-//        let url = URL(string: "https://split-realtime.ably.io/sse")
-//        let target = DynamicTarget(u)
-//        let r = httpClient?.sendRequest(target: <#T##Target##Split.Target#>)
+        let PUSH_NOTIFICATION_CHANNELS_PARAM = "channel"
+        let PUSH_NOTIFICATION_TOKEN_PARAM = "accessToken"
+        let PUSH_NOTIFICATION_VERSION_PARAM = "v"
+        let PUSH_NOTIFICATION_VERSION_VALUE = "1.1"
+        let sseEndpoint = EndpointFactory(serviceEndpoints: <#T##ServiceEndpoints##Split.ServiceEndpoints#>.builder().build(),
+                apiKey: IntegrationHelper.dummyApiKey, userKey: IntegrationHelper.dummyUserKey).streamingEndpoint
+
+        let r = httpClient?.sendRequest(target: sseEndpoint).getResponse(errorSanitizer: <#T##@escaping (JSON, Int) -> HttpResult<JSON>##@escaping (Split.JSON, Swift.Int) -> Split.HttpResult<Split.JSON>#>, completionHandler: <#T##@escaping (HttpDataResponse<JSON>) -> Void##@escaping (Split.HttpDataResponse<Split.JSON>) -> Swift.Void#>)
+
+        _ = httpClient.sendRequest(
+                        endpoint: sseEndpoint,
+                        parameters: parameters,
+                        headers: nil,
+                        body: body)
+                .getResponse(errorSanitizer: endpoint.errorSanitizer) { response in
+                    switch response.result {
+                    case .success(let json):
+                        if json.isNull() {
+                            completion(DataResult { return nil })
+                            return
+                        }
+
+                        do {
+                            let parsedObject = try json.decode(T.self)
+                            completion(DataResult { return parsedObject })
+                        } catch {
+                            completion(DataResult { throw error })
+                        }
+                    case .failure(let error):
+                        completion(DataResult { throw error })
+                    }
+
     }
 }
