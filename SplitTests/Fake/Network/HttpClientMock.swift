b2b2a333f041831b3f4570449199a786e9fac336
Javier L. Avrudsky <javier.avrudsky@black-tobacco.com>
2020-07-13 13:31:52 -0300

    Added test harness components for sse client

---
+++ SplitTests/Fake/Network/HttpClientMock.swift
@@ -0,0 +1,14 @@
+//
+//  HttpClientMock.swift
+//  SplitTests
+//
+//  Created by Javier L. Avrudsky on 13/07/2020.
+//  Copyright © 2020 Split. All rights reserved.
+//
+
+import Foundation
+@import Split
+
+class HttpClientMock: HttpClient {
+
+}
