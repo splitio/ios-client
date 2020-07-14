92e779d96688b7b0ab7e50253333e66ead566e6a
Javier L. Avrudsky <javier.avrudsky@black-tobacco.com>
2020-07-14 17:47:55 -0300

    Added SSE client base implementation and tests

--- SplitTests/Fake/Network/HttpStreamRequestMock.swift
+++ SplitTests/Fake/Network/HttpStreamRequestMock.swift
@@ -14,13 +14,50 @@ class HttpStreamRequestMock: HttpStreamRequest {
     var responseHandler: ResponseHandler?
     var incomingDataHandler: IncomingDataHandler?
     var closeHandler: CloseHandler?
+    var errorHandler: ErrorHandler?
 
-    func getResponse(responseHandler: @escaping ResponseHandler,
-                     incomingDataHandler: @escaping IncomingDataHandler,
-                     closeHandler: @escaping CloseHandler) -> Self {
+    var identifier: Int = 0
+
+    var url: URL = URL(string: "www.split.com")!
+
+    var method: HttpMethod = .get
+
+    var parameters: HttpParameters?
+
+    var headers: HttpHeaders = [:]
+
+    var body: Data?
+
+    var responseCode: Int = 0
+
+    func send() {
+    }
+
+    func setResponse(code: Int) {
+        if let handler = responseHandler {
+            handler(HttpResponse(code: code))
+        }
+    }
+
+    func notifyIncomingData(_ data: Data) {
+        if let handler = incomingDataHandler {
+            handler(data)
+        }
+
+    }
+
+    func complete(error: HttpError?) {
+        if let handler = closeHandler {
+            handler()
+        }
+    }
+
+    func getResponse(responseHandler: @escaping ResponseHandler, incomingDataHandler: @escaping IncomingDataHandler, closeHandler: @escaping CloseHandler, errorHandler: @escaping ErrorHandler) -> Self {
         self.responseHandler = responseHandler
         self.incomingDataHandler = incomingDataHandler
         self.closeHandler = closeHandler
+        self.errorHandler = errorHandler
+        return self
     }
 
 }
