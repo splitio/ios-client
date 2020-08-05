0128e9a3d0c47dc1d0d072168b0c7ad8e6837d5f
Javier L. Avrudsky <javier.avrudsky@black-tobacco.com>
2020-08-05 15:03:30 -0300

    Reusing function to validate split name

--- Split/Common/Validators/SplitValidator.swift
+++ Split/Common/Validators/SplitValidator.swift
@@ -62,23 +62,7 @@ class DefaultSplitValidator: SplitValidator {
     }
 
     func validate(name: String?) -> ValidationErrorInfo? {
-
-        if name == nil {
-            return ValidationErrorInfo(error: .some,
-                                       message: "you passed a null split name, split name must be a non-empty string")
-        }
-
-        if name!.isEmpty() {
-            return ValidationErrorInfo(error: .some,
-                                       message: "you passed an empty split name, split name must be a non-empty string")
-        }
-
-        if name!.trimmingCharacters(in: .whitespacesAndNewlines) != name! {
-            return ValidationErrorInfo(warning: .splitNameShouldBeTrimmed,
-                                       message: "split name '\(name!)' has extra whitespace, trimming")
-        }
-
-        return nil
+        return splitNameValidator.validate(name: name)
     }
 
     func validateSplit(name: String) -> ValidationErrorInfo? {
