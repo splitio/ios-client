3b39652326b0167403f0e3f27e92f3ddca398849
Javier L. Avrudsky <javier.avrudsky@black-tobacco.com>
2020-08-14 16:25:03 -0300

    Updated http headers case

--- SplitTests/HttpClient/EndpointFactoryTest.swift
+++ SplitTests/HttpClient/EndpointFactoryTest.swift
@@ -12,9 +12,9 @@ import XCTest
 
 class EndpointFactoryTest: XCTestCase {
 
-    private let kAuthorizationHeader = "authorization"
-    private let kSplitVersionHeader = "splitsdkversion"
-    private let kContentTypeHeader = "content-type"
+    private let kAuthorizationHeader = "Authorization"
+    private let kSplitVersionHeader = "SplitsSDKVersion"
+    private let kContentTypeHeader = "Content-Type"
     private let kAuthorizationBearer = "Bearer \(CommonValues.apiKey)"
     private let kContentTypeJson = "application/json"
     private let kContentTypeEventStream = "text/event-stream"
