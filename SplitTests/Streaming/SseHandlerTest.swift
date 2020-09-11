ec78faf4ffa3e34b30d0b5112c83c869607935b5
Javier L. Avrudsky <javier.avrudsky@black-tobacco.com>
2020-09-11 18:06:58 -0300

    Fixing test data

--- SplitTests/Streaming/SseHandlerTest.swift
+++ SplitTests/Streaming/SseHandlerTest.swift
@@ -68,7 +68,7 @@ class SseHandlerTest: XCTestCase {
     }
 
     func testIncomingControlStreamingPaused() {
-        notificationParser.incomingNotification = IncomingNotification(type: .control, jsonData: "dummy")
+        notificationParser.incomingNotification = IncomingNotification(type: .control, jsonData: "dummy", timestamp: 100)
         notificationParser.controlNotification = ControlNotification(type: .control, controlType: .streamingPaused)
         sseHandler.handleIncomingMessage(message: ["data": "{pepe}"])
 
@@ -76,7 +76,7 @@ class SseHandlerTest: XCTestCase {
     }
 
     func testIncomingControlStreamingDisabled() {
-        notificationParser.incomingNotification = IncomingNotification(type: .control, jsonData: "dummy")
+        notificationParser.incomingNotification = IncomingNotification(type: .control, jsonData: "dummy", timestamp: 100)
         notificationParser.controlNotification = ControlNotification(type: .control, controlType: .streamingDisabled)
         sseHandler.handleIncomingMessage(message: ["data": "{pepe}"])
 
@@ -85,7 +85,7 @@ class SseHandlerTest: XCTestCase {
 
     func testIncomingControlStreamingEnabled() {
         notificationManagerKeeper.publishersCount = 1
-        notificationParser.incomingNotification = IncomingNotification(type: .control, jsonData: "dummy")
+        notificationParser.incomingNotification = IncomingNotification(type: .control, jsonData: "dummy", timestamp: 100)
         notificationParser.controlNotification = ControlNotification(type: .control, controlType: .streamingEnabled)
         sseHandler.handleIncomingMessage(message: ["data": "{pepe}"])
 
