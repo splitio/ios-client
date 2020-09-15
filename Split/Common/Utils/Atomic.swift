5a28e1d6bf2f78aee119d7d1430f156adb550e0f
Javier L. Avrudsky <javier.avrudsky@black-tobacco.com>
2020-09-15 10:47:02 -0300

    Added some status checks to prevent some possible unexpected behavior

--- Split/Common/Utils/Atomic.swift
+++ Split/Common/Utils/Atomic.swift
@@ -8,15 +8,54 @@
 
 import Foundation
 
+final class Atomic<T> {
+    private let queue = DispatchQueue.global()
+    private var currentValue: T
+    init(_ value: T) {
+        self.currentValue = value
+    }
+
+    var value: T {
+            return queue.sync { self.currentValue }
+    }
+
+    func mutate(_ transformation: (inout T) -> Void) {
+        queue.sync {
+            transformation(&self.currentValue)
+        }
+    }
+
+    func mutate(_ transformation: (T, inout T) -> Void) {
+        queue.sync {
+            transformation(currentValue, &self.currentValue)
+        }
+    }
+
+    func getAndSet(_ newValue: T) -> T {
+        var oldValue: T!
+        queue.sync {
+            oldValue = self.currentValue
+            self.currentValue = newValue
+        }
+        return oldValue
+    }
+
+    func set(_ newValue: T) {
+        queue.sync {
+            self.currentValue = newValue
+        }
+    }
+}
+
 final class AtomicInt {
-    private let queue = DispatchQueue(label: "split_atomic_int")
+    private let queue = DispatchQueue.global()
     private var curValue: Int
     init(_ value: Int) {
         self.curValue = value
     }
 
     var value: Int {
-            return queue.sync { self.curValue }
+        return queue.sync { self.curValue }
     }
 
     func getAndAdd(_ addValue: Int) -> Int {
