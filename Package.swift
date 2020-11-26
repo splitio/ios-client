3ee3dbcc7fe29eb61024a4e8ed5badd31dfec516
Ben Sarrazin <b@srz.io>
2020-11-25 21:10:13 -0500

    Adding SPM support

---
+++ Package.swift
@@ -0,0 +1,31 @@
+// swift-tools-version:5.3
+
+import PackageDescription
+
+let package = Package(
+    name: "Split",
+    platforms: [.iOS(.v9)],
+    products: [
+        .library(
+            name: "Split",
+            targets: ["Split"]
+        )
+    ],
+    targets: [
+        .target(
+            name: "Split",
+            dependencies: ["JFBCrypt"],
+            path: "Split",
+            exclude: [
+                "Common/Utils/JFBCrypt/",
+                "Common/Yaml/LICENSE",
+                "Info.plist",
+                "Split.h"
+            ]
+        ),
+        .target(
+            name: "JFBCrypt",
+            path: "Split/Common/Utils/JFBCrypt"
+        )
+    ]
+)
