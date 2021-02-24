169cb2bb13b1b388b535d028bf12169462256597
Javier L. Avrudsky <javier.avrudsky@split.io>
2021-02-24 09:57:44 -0300

    Improved possible race condition in Backoff counter timer

--- Split/Network/Streaming/BackoffCounterTimer.swift
+++ Split/Network/Streaming/BackoffCounterTimer.swift
@@ -15,6 +15,7 @@ protocol BackoffCounterTimer {
 
 class DefaultBackoffCounterTimer: BackoffCounterTimer {
     private let reconnectBackoffCounter: ReconnectBackoffCounter
+    private let queue = DispatchQueue(label: "split-backoff-timer")
     private let timersQueue = DispatchQueue.global()
     private var workItem: DispatchWorkItem?
     private var isScheduled: Atomic<Bool> = Atomic(false)
@@ -24,6 +25,20 @@ class DefaultBackoffCounterTimer: BackoffCounterTimer {
     }
 
     func schedule(handler: @escaping () -> Void) {
+        queue.async {
+            self.schedule(handler)
+        }
+    }
+
+    func cancel() {
+        queue.async {
+            self.workItem?.cancel()
+            self.workItem = nil
+            self.reconnectBackoffCounter.resetCounter()
+        }
+    }
+
+    private func schedule(_ handler: @escaping () -> Void) {
         if workItem != nil, isScheduled.getAndSet(true) {
             return
         }
@@ -36,11 +51,5 @@ class DefaultBackoffCounterTimer: BackoffCounterTimer {
         Logger.d("Retrying reconnection in \(delayInSeconds) seconds")
         timersQueue.asyncAfter(deadline: DispatchTime.now() + Double(delayInSeconds), execute: workItem)
         self.workItem = workItem
-    }
-
-    func cancel() {
-        workItem?.cancel()
-        workItem = nil
-        reconnectBackoffCounter.resetCounter()
     }
 }
