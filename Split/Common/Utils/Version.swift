fe8488067b495907a9f2bfbb63899778feca9ca2
Javier L. Avrudsky <javier.avrudsky@split.io>
2021-02-26 15:07:12 -0300

    Version 2.9.0 - Cache revamp

--- Split/Common/Utils/Version.swift
+++ Split/Common/Utils/Version.swift
@@ -9,7 +9,7 @@ import Foundation
 
 class Version {
     private static let kSdkPlatform: String = "ios"
-    private static let kVersion = "2.9.0-rc11"
+    private static let kVersion = "2.9.0"
 
     static var semantic: String {
         return kVersion
