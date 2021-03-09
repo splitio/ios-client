b77da6d7ddbab4a7e57f77b662493437cef0ff57
Javier L. Avrudsky <javier.avrudsky@split.io>
2021-03-09 11:09:02 -0300

    Added background sync component

--- Split/Network/Streaming/BackgroundSynchronizer.swift
+++ Split/Network/Streaming/BackgroundSynchronizer.swift
@@ -9,5 +9,37 @@
 import Foundation
 
 public class BackgroundSynchronizer {
-    
+
+    let splitsSyncWorker: BackgroundSynchronizer
+    let mySegmentsSyncWorker: BackgroundSynchronizer
+    let eventsRecorderWorker: RecorderWorker
+    let impressionsRecorderWorker: RecorderWorker
+
+    init(splitsSyncWorker: BackgroundSynchronizer, mySegmentsSyncWorker: BackgroundSynchronizer,
+         eventsRecorderWorker: RecorderWorker, impressionsRecorderWorker: RecorderWorker) {
+        self.splitsSyncWorker = splitsSyncWorker
+        self.mySegmentsSyncWorker = mySegmentsSyncWorker
+        self.eventsRecorderWorker = eventsRecorderWorker
+        self.impressionsRecorderWorker = impressionsRecorderWorker
+    }
+
+    func executeSync() {
+        let operationQueue = OperationQueue()
+
+        operationQueue.addOperation {
+            self.splitsSyncWorker.executeSync()
+        }
+
+        operationQueue.addOperation {
+            self.mySegmentsSyncWorker.executeSync()
+        }
+
+        operationQueue.addOperation {
+            self.eventsRecorderWorker.flush()
+        }
+
+        operationQueue.addOperation {
+            self.impressionsRecorderWorker.flush()
+        }
+    }
 }
