0d51bfb7a26da21fb754a4d0bd78dce1003d32af
Javier L. Avrudsky <javier.avrudsky@split.io>
2021-03-31 09:49:00 -0300

    Added bg sync registration map and functions to handle it

--- Split/Network/Streaming/SplitBackgroundSynchronizer.swift
+++ Split/Network/Streaming/SplitBackgroundSynchronizer.swift
@@ -9,28 +9,58 @@
 import Foundation
 import BackgroundTasks
 
-
 @objc public class SplitBackgroundSynchronizer: NSObject {
 
     @objc public static let shared = SplitBackgroundSynchronizer()
 
+    private struct SyncItem: Codable {
+        let apiKey: String
+        var userKeys: Set<String> = Set()
+    }
 
+    private typealias BgSyncSchedule = [String: SyncItem]
     private let taskId = "io.split.bg-sync.task"
-    private static let kTimeInterval = 15.0 * 60
+    private static let kTimeInterval = ServiceConstants.backgroundSyncPeriod
+    var globalStorage: KeyValueStorage = GlobalSecureStorage.shared
 
+    @objc public func register(apiKey: String, userKey: String) {
+        var syncMap = getSyncTaskMap()
+        var syncItem = syncMap[apiKey] ?? SyncItem(apiKey: apiKey)
+        syncItem.userKeys.insert(userKey)
+        syncMap[apiKey] = syncItem
+        globalStorage.set(item: syncMap, for: .backgroundSyncSchedule)
+    }
 
+    @objc public func unregister(apiKey: String, userKey: String) {
+        let syncMap = getSyncTaskMap()
+        if var item = syncMap[apiKey], item.userKeys.contains(userKey) == true {
+            item.userKeys.remove(userKey)
+            globalStorage.set(item: syncMap, for: .backgroundSyncSchedule)
+        }
+    }
+
+    @objc public func unregisterAll() {
+        globalStorage.remove(item: .backgroundSyncSchedule)
+    }
 
     // keys = [ApiKey: UserKey]
-    @objc public func schedule(keys: [String: String],
-                               serviceEndpoints: ServiceEndpoints? = nil) {
+    @objc public func schedule(serviceEndpoints: ServiceEndpoints? = nil) {
         if #available(iOS 13.0, *) {
+
             BGTaskScheduler.shared.register(
-                forTaskWithIdentifier: taskId,
-                using: nil) { task in
+                forTaskWithIdentifier: taskId, using: nil) { task in
+                let syncList = self.getSyncTaskMap()
+                if syncList.count == 0 {
+                    return
+                }
                 let operationQueue = OperationQueue()
-                for (apiKey, userKey) in keys {
-                    let executor = BackgroundSyncExecutor(apiKey: apiKey, userKey: userKey)
-                    executor.execute(operationQueue: operationQueue)
+                for item in syncList.values {
+                    do {
+                        let executor = try BackgroundSyncExecutor(apiKey: item.apiKey, userKeys: Array(item.userKeys))
+                        executor.execute(operationQueue: operationQueue)
+                    } catch {
+                        Logger.d("Could not create background synchronizer for api key: \(item.apiKey)")
+                    }
                 }
 
                 task.expirationHandler = {
@@ -62,30 +92,41 @@ import BackgroundTasks
             }
         }
     }
+
+    private func getSyncTaskMap() -> [String: SyncItem] {
+        return globalStorage.get(item: .backgroundSyncSchedule, type: BgSyncSchedule.self) ?? [String: SyncItem]()
+    }
 }
 
 struct BackgroundSyncExecutor {
-    private var splitsSyncWorker: BackgroundSyncWorker?
-    private var mySegmentsSyncWorker: BackgroundSyncWorker?
-    private var eventsRecorderWorker: RecorderWorker?
-    private var impressionsRecorderWorker: RecorderWorker?
+    private let splitDatabase: SplitDatabase
+    private let splitsSyncWorker: BackgroundSyncWorker
+    private let eventsRecorderWorker: RecorderWorker
+    private let impressionsRecorderWorker: RecorderWorker
+    private let userKeys: [String]
+    private let mySegmentsFetcher: HttpMySegmentsFetcher
 
-    init(apiKey: String, userKey: String,
-                serviceEndpoints: ServiceEndpoints? = nil) {
+    init(apiKey: String, userKeys: [String],
+         serviceEndpoints: ServiceEndpoints? = nil) throws {
 
+        self.userKeys = userKeys
         let dataFolderName = DataFolderFactory().createFrom(apiKey: apiKey) ?? ServiceConstants.defaultDataFolder
 
+        // Using "" as key because My Segment storage property will not be used
+        // TODO: Update storage container when using multipley key for a client
         guard let storageContainer = try? SplitFactoryHelper.buildStorageContainer(
-                userKey: userKey, dataFolderName: dataFolderName, testDatabase: nil) else {
-            return
+                userKey: "", dataFolderName: dataFolderName, testDatabase: nil) else {
+            throw GenericError.unknown(message: "Could not create storage container")
         }
+
+        splitDatabase = storageContainer.splitDatabase
 
         let splitsFilterQueryString = storageContainer.splitDatabase
             .generalInfoDao.stringValue(info: .splitsFilterQueryString) ?? ""
 
         let endpoints = serviceEndpoints ?? ServiceEndpoints.builder().build()
         let  endpointFactory = EndpointFactory(serviceEndpoints: endpoints,
-                                               apiKey: apiKey, userKey: userKey,
+                                               apiKey: apiKey,
                                                splitsQueryString: splitsFilterQueryString)
 
         let restClient = DefaultRestClient(httpClient: DefaultHttpClient.shared,
@@ -95,18 +136,14 @@ struct BackgroundSyncExecutor {
         let splitsFetcher = DefaultHttpSplitFetcher(restClient: restClient,
                                                     metricsManager: DefaultMetricsManager.shared)
 
-        let mySegmentsFetcher: HttpMySegmentsFetcher
-            = DefaultHttpMySegmentsFetcher(restClient: restClient, metricsManager: DefaultMetricsManager.shared)
+        self.mySegmentsFetcher = DefaultHttpMySegmentsFetcher(restClient: restClient,
+                                                              metricsManager: DefaultMetricsManager.shared)
 
         let cacheExpiration = Int64(ServiceConstants.cacheExpirationInSeconds)
         self.splitsSyncWorker = BackgroundSplitsSyncWorker(splitFetcher: splitsFetcher,
                                                            splitsStorage: storageContainer.splitsStorage,
                                                            splitChangeProcessor: DefaultSplitChangeProcessor(),
                                                            cacheExpiration: cacheExpiration)
-
-        self.mySegmentsSyncWorker = BackgroundMySegmentsSyncWorker(
-            userKey: userKey, mySegmentsFetcher: mySegmentsFetcher,
-            mySegmentsStorage: storageContainer.mySegmentsStorage)
 
         let impressionsRecorder = DefaultHttpImpressionsRecorder(restClient: restClient)
         let eventsRecorder = DefaultHttpEventsRecorder(restClient: restClient)
@@ -123,19 +160,30 @@ struct BackgroundSyncExecutor {
     func execute(operationQueue: OperationQueue) {
 
         operationQueue.addOperation {
-            self.splitsSyncWorker?.execute()
+            self.splitsSyncWorker.execute()
         }
 
         operationQueue.addOperation {
-            self.mySegmentsSyncWorker?.execute()
+            for userKey in self.userKeys {
+                let persistentMySegmentsStorage =
+                    DefaultPersistentMySegmentsStorage(userKey: userKey,
+                                                       database: self.splitDatabase)
+                let mySegmentsStorage =
+                    DefaultMySegmentsStorage(persistentMySegmentsStorage: persistentMySegmentsStorage)
+
+                let mySegmentsSyncWorker = BackgroundMySegmentsSyncWorker(
+                    userKey: userKey, mySegmentsFetcher: self.mySegmentsFetcher,
+                    mySegmentsStorage: mySegmentsStorage)
+                mySegmentsSyncWorker.execute()
+            }
         }
 
         operationQueue.addOperation {
-            self.eventsRecorderWorker?.flush()
+            self.eventsRecorderWorker.flush()
         }
 
         operationQueue.addOperation {
-            self.impressionsRecorderWorker?.flush()
+            self.impressionsRecorderWorker.flush()
         }
     }
 }
