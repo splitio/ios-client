01ab6711e9983e66a0bf82a1d35f8b5c7a21efc3
Javier L. Avrudsky <javier.avrudsky@split.io>
2021-05-07 14:16:33 -0300

    Adding test for interrupt method

--- SplitTests/BlockingQueueTest.swift
+++ SplitTests/BlockingQueueTest.swift
@@ -23,10 +23,13 @@ class BlockingQueueTest: XCTestCase {
 
         globalQ.async {
             while true {
-                let event = queue.take()
-                local.append(event)
-                if local.count == 4 {
-                    endExp.fulfill()
+                do {
+                    let event = try queue.take()
+                    local.append(event)
+                    if local.count == 4 {
+                        endExp.fulfill()
+                    }
+                } catch {
                 }
             }
         }
@@ -44,7 +47,41 @@ class BlockingQueueTest: XCTestCase {
         XCTAssertEqual(SplitInternalEvent.mySegmentsUpdated, local[1])
         XCTAssertEqual(SplitInternalEvent.mySegmentsLoadedFromCache, local[2])
         XCTAssertEqual(SplitInternalEvent.splitsLoadedFromCache, local[3])
+    }
 
+    func testInterrupt() {
+        let endExp = XCTestExpectation()
+        var local = [SplitInternalEvent]()
+        let globalQ = DispatchQueue.global()
+        let queue = DefaultInternalEventBlockingQueue()
+        var interrupted = false
+
+        globalQ.async {
+            while true {
+                do {
+                    let event = try queue.take()
+                    local.append(event)
+                } catch {
+                    endExp.fulfill()
+                    interrupted = true
+                }
+            }
+        }
+        globalQ.asyncAfter(deadline: .now() + 1) {
+            queue.add(SplitInternalEvent.mySegmentsLoadedFromCache)
+            globalQ.asyncAfter(deadline: .now() + 1) {
+                queue.interrupt()
+            }
+        }
+
+        queue.add(SplitInternalEvent.splitsUpdated)
+        queue.add(SplitInternalEvent.mySegmentsUpdated)
+
+        wait(for: [endExp], timeout: 10)
+        XCTAssertEqual(SplitInternalEvent.splitsUpdated, local[0])
+        XCTAssertEqual(SplitInternalEvent.mySegmentsUpdated, local[1])
+        XCTAssertEqual(SplitInternalEvent.mySegmentsLoadedFromCache, local[2])
+        XCTAssertTrue(interrupted)
     }
 
     override func tearDown() {
