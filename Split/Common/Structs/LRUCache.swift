bcf5fa3d8064f0cdae64142fc9d1791b87a89b80
Javier L. Avrudsky <javier.avrudsky@split.io>
2021-06-16 18:06:11 -0300

    Added impressions observer hashear and tests

---
+++ Split/Common/Structs/LRUCache.swift
@@ -0,0 +1,80 @@
+//
+//  LRUCache.swift
+//  Split
+//
+//  Created by Javier Avrudsky on 16/06/2021.
+//  Copyright © 2021 Split. All rights reserved.
+//
+
+import Foundation
+
+class LRUCache<K: Hashable, E> {
+    /// Elements queue tracks objects usage
+    /// first element is last used
+    /// last element is least used
+    private var elementsQueue: [K]
+    private var elements: [K: E]
+    private let capacity: Int
+    private let queue: DispatchQueue
+
+    init(capacity: Int) {
+        self.capacity = capacity
+        self.elements = [K: E]()
+        self.elementsQueue = [K]()
+        self.elementsQueue.reserveCapacity(capacity)
+        self.queue = DispatchQueue(label: "split-lru", attributes: .concurrent)
+    }
+
+    func set(_ element: E, for key: K) {
+        queue.async(flags: .barrier) {
+            self.put(element, for: key)
+        }
+    }
+
+    func element(for key: K) -> E? {
+        var element: E?
+        queue.sync {
+            element = self.get(for: key)
+        }
+        return element
+    }
+
+    // Private function to avoid using self
+    // Call this functions only from within queue closure
+    private func put(_ element: E, for key: K) {
+
+        // If element exists, remove from current position in the queue
+        // to add last after
+        if elements[key] != nil, let index = elementsQueue.firstIndex(where: { $0 == key }) {
+            elementsQueue.remove(at: index)
+        }
+
+        // Add new element for key
+        elements[key] = element
+
+        // Check capacity before adding to avoid
+        // reserving more memory
+        if elements.count > capacity {
+            // Remove least used
+            let keyToRemove = elementsQueue.removeLast()
+            elements.removeValue(forKey: keyToRemove)
+        }
+
+        // Add as last used
+        elementsQueue.insert(key, at: 0)
+    }
+
+    // Same as above
+    private func get(for key: K) -> E? {
+        // Get element by key. Returns nil if not found
+        guard let element = elements[key] else {
+            return nil
+        }
+        // If element exists, move as last used
+        if let index = elementsQueue.firstIndex(where: { $0 == key }) {
+            elementsQueue.remove(at: index)
+            elementsQueue.insert(key, at: 0)
+        }
+        return element
+    }
+}
---
+++ Split/Impressions/ImpressionsObserver.swift
@@ -0,0 +1,40 @@
+//
+//  ImpressionsObserver.swift
+//  Split
+//
+//  Created by Javier Avrudsky on 15/06/2021.
+//  Copyright © 2021 Split. All rights reserved.
+//
+
+import Foundation
+
+struct ImpressionsObserver {
+    private let cache: LRUCache<UInt32, Int64>
+
+    init(size: Int) {
+        cache = LRUCache(capacity: size)
+    }
+
+    func testAndSet(impression: Impression) -> Int64? {
+        let hash = ImpressionHasher.process(impression: impression)
+        let previous = cache.element(for: hash)
+        cache.set(impression.time ?? 0, for: hash)
+        if previous == nil {
+            return nil
+        }
+        return min(previous ?? 0, impression.time ?? 0)
+    }
+}
+
+struct ImpressionHasher {
+
+    private static let kUnknown = "UNKNOWN"
+    private static let kSeed: UInt32 = 0
+    private static let kOffset: Int64 = 0
+
+    static func process(impression: Impression) -> UInt32 {
+        let data = "\(impression.keyName ?? ""):\(impression.feature ?? ""):\(impression.treatment ?? ""):" +
+            "\(impression.label ?? ""):\(impression.changeNumber ?? 0)"
+        return Murmur3Hash.hashString(data, Self.kSeed)
+    }
+}
