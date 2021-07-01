a39771e922a718d0fed7fa1e2233c9eb856649d6
Javier L. Avrudsky <javier.avrudsky@split.io>
2021-07-01 17:57:51 -0300

    Fixing issue on localhost returning control when loading splits

--- Split/FetcherEngine/Refresh/YamlSplitsStorage.swift
+++ Split/FetcherEngine/Refresh/YamlSplitsStorage.swift
@@ -14,14 +14,11 @@ struct YamlSplitStorageConfig {
 
 class YamlSplitsStorage: SplitsStorage {
 
-    
+    var changeNumber: Int64 = -1
+    var updateTimestamp: Int64 = 1
+    var splitsFilterQueryString: String = ""
 
-    var changeNumber: Int64
-
-    var updateTimestamp: Int64
-
-    var splitsFilterQueryString: String
-
+    typealias FileInfo = (name: String, type: String)
     private let refreshInterval: Int
     private let eventsManager: SplitEventsManager
     private let fileStorage: FileStorageProtocol
@@ -30,6 +27,7 @@ class YamlSplitsStorage: SplitsStorage {
     private let supportedExtensions = ["yaml", "yml", "splits"]
     private let fileName: String
     private let inMemorySplits = SyncDictionarySingleWrapper<String, Split>()
+    private let dataQueue = DispatchQueue(label: "Split yaml storage queue", attributes: .concurrent)
 
     init(fileStorage: FileStorageProtocol,
          config: YamlSplitStorageConfig = YamlSplitStorageConfig(),
@@ -40,65 +38,40 @@ class YamlSplitsStorage: SplitsStorage {
         self.fileStorage = fileStorage
         self.refreshInterval = config.refreshInterval
         self.eventsManager = eventsManager
-        self.changeNumber = -1
-        self.updateTimestamp = 1
-        self.splitsFilterQueryString = ""
 
-        let fileName = splitsFileName
-        guard let fileInfo = splitFileName(fileName) else {
-            eventsManager.notifyInternalEvent(SplitInternalEvent.sdkReadyTimeoutReached)
-            Logger.e("""
-                Localhost file name \(fileName) has not the correct format.
-                It should be similar to 'name.yaml', 'name.yml'
-                """)
-            return
-        }
-
-        if !isSupportedExtensionType(fileInfo.type) {
-            eventsManager.notifyInternalEvent(SplitInternalEvent.sdkReadyTimeoutReached)
-            Logger.e("Localhost file extension \(fileInfo.type) is not supported. It should be '.yaml', '.yml'")
-            return
-        }
-
-        if !LocalhostFileCopier(bundle: bundle).copySourceFile(name: fileInfo.name,
-                                                               type: fileInfo.type,
-                                                               fileStorage: fileStorage) {
-            eventsManager.notifyInternalEvent(SplitInternalEvent.sdkReadyTimeoutReached)
-            Logger.e("Localhost file name \(fileName) not found. Please check name.")
-            return
-        }
-
-        self.fileParser = parser(for: fileInfo.type)
-        if !loadFile(name: fileName) {
-            eventsManager.notifyInternalEvent(SplitInternalEvent.sdkReadyTimeoutReached)
-            Logger.e("Localhost file \(fileName) not found or empty.")
-            return
-        }
-
-        logFileInfo(dataFolder: dataFolderName, name: fileName)
-        eventsManager.notifyInternalEvent(.mySegmentsUpdated)
-        eventsManager.notifyInternalEvent(.splitsUpdated)
-        if refreshInterval > 0 {
-            self.taskExecutor = createTaskExecutor()
-            self.start()
-        }
+        initSdk(bundle: bundle, dataFolderName: dataFolderName)
     }
 
     func loadLocal() {
-        loadFile(name: fileName)
+        dataQueue.async(flags: .barrier) {
+            self.loadFile(name: self.fileName)
+            Logger.d("Localhost splits updated")
+        }
     }
 
     func get(name: String) -> Split? {
-        inMemorySplits.value(forKey: name)
+        var split: Split?
+        dataQueue.sync {
+            split = inMemorySplits.value(forKey: name)
+        }
+        return split
     }
 
     func getMany(splits: [String]) -> [String: Split] {
-        let names = Set(splits)
-        return inMemorySplits.all.filter { return names.contains($0.key) }
+        var splitMap: [String: Split]?
+        dataQueue.sync {
+            let names = Set(splits)
+            splitMap = self.inMemorySplits.all.filter { return names.contains($0.key) }
+        }
+        return splitMap ?? [String: Split]()
     }
 
     func getAll() -> [String: Split] {
-        return inMemorySplits.all
+        var splitMap: [String: Split]?
+        dataQueue.sync {
+            splitMap = self.inMemorySplits.all
+        }
+        return splitMap ?? [String: Split]()
     }
 
     func update(splitChange: ProcessedSplitChange) {
@@ -115,7 +88,9 @@ class YamlSplitsStorage: SplitsStorage {
     }
 
     func clear() {
-        inMemorySplits.removeAll()
+        dataQueue.async(flags: .barrier) {
+            self.inMemorySplits.removeAll()
+        }
     }
 
     func start() {
@@ -130,34 +105,15 @@ class YamlSplitsStorage: SplitsStorage {
         var config = PeriodicTaskExecutorConfig()
         config.firstExecutionWindow = refreshInterval
         config.rate = refreshInterval
-        let fileName = self.fileName
         return PeriodicTaskExecutor(
             dispatchGroup: nil,
             config: config,
             triggerAction: {[weak self] in
                 if let self = self {
-                    self.loadFile(name: fileName)
+                    self.loadLocal()
                 }
             }
         )
-    }
-
-    @discardableResult
-    private func loadFile(name: String) -> Bool {
-
-        inMemorySplits.removeAll()
-        guard let content = fileStorage.read(fileName: name), let parser = self.fileParser else {
-            return false
-        }
-
-        let loadedSplits = parser.parseContent(content)
-
-        if loadedSplits.count < 1 {
-            return false
-        }
-
-        inMemorySplits.setValues(loadedSplits)
-        return true
     }
 
     private func parser(for type: String) -> LocalhostSplitsParser {
@@ -178,7 +134,7 @@ class YamlSplitsStorage: SplitsStorage {
         Logger.i("Localhost file path: \(path)")
     }
 
-    private func splitFileName(_ fileName: String) -> (name: String, type: String)? {
+    private func splitFileName(_ fileName: String) -> FileInfo? {
         if let dotIndex = fileName.lastIndex(of: ".") {
             let name = String(fileName.prefix(upTo: dotIndex))
             let type = String(fileName.suffix(from: fileName.index(after: dotIndex)))
@@ -191,6 +147,90 @@ class YamlSplitsStorage: SplitsStorage {
 
     private func isSupportedExtensionType(_ type: String) -> Bool {
         return supportedExtensions.filter({ $0 == type.lowercased() }).count == 1
+    }
+
+    private func validateAndCopyFile(bundle: Bundle) -> FileInfo? {
+        guard let fileInfo = splitFileName(fileName) else {
+            Logger.e("""
+                Localhost file name \(fileName) has not the correct format.
+                It should be similar to 'name.yaml', 'name.yml'
+                """)
+            return nil
+        }
+
+        if !isSupportedExtensionType(fileInfo.type) {
+            Logger.e("Localhost file extension \(fileInfo.type) is not supported. It should be '.yaml', '.yml'")
+            return nil
+        }
+
+        if !LocalhostFileCopier(bundle: bundle).copySourceFile(name: fileInfo.name,
+                                                               type: fileInfo.type,
+                                                               fileStorage: fileStorage) {
+            Logger.e("Localhost file name \(fileName) not found. Please check name.")
+            return nil
+        }
+        return fileInfo
+    }
+
+    private func initSdk(bundle: Bundle, dataFolderName: String) {
+        dataQueue.async(flags: .barrier) {
+            self.initSdkSync(bundle: bundle, dataFolderName: dataFolderName)
+
+        }
+    }
+
+    // Must be called from initSdk. Just to avoid using self many times
+    private func initSdkSync(bundle: Bundle, dataFolderName: String) {
+        guard let fileInfo = validateAndCopyFile(bundle: bundle) else {
+            triggerSdkTimeout()
+            return
+        }
+
+        fileParser = parser(for: fileInfo.type)
+        if !loadFile(name: fileName) {
+            triggerSdkTimeout()
+            Logger.e("Localhost file \(fileName) not found or empty.")
+            return
+        }
+
+        logFileInfo(dataFolder: dataFolderName, name: fileName)
+        triggerSdkReady()
+        startPeriodicRefresh()
+    }
+
+    // Must be called from initSdk. Just to avoid using self many times
+    @discardableResult
+    private func loadFile(name: String) -> Bool {
+
+        inMemorySplits.removeAll()
+        guard let content = fileStorage.read(fileName: name), let parser = self.fileParser else {
+            return false
+        }
+
+        let loadedSplits = parser.parseContent(content)
+
+        if loadedSplits.count < 1 {
+            return false
+        }
+
+        inMemorySplits.setValues(loadedSplits)
+        return true
+    }
+
+    private func triggerSdkTimeout() {
+        eventsManager.notifyInternalEvent(SplitInternalEvent.sdkReadyTimeoutReached)
+    }
+
+    private func triggerSdkReady() {
+        eventsManager.notifyInternalEvent(.mySegmentsUpdated)
+        eventsManager.notifyInternalEvent(.splitsUpdated)
+    }
+
+    private func startPeriodicRefresh() {
+        if refreshInterval > 0 {
+            self.taskExecutor = createTaskExecutor()
+            self.start()
+        }
     }
 }
 
