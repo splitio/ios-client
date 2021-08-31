3d12760d36fb3ac2927aad4b52ecdca2e761ed6e
Javier L. Avrudsky <javier.avrudsky@split.io>
2021-08-31 10:33:20 -0300

    Added bit rotate functions for UInt64

--- Split/Common/Extensions/UInt64+bits.swift
+++ Split/Common/Extensions/UInt64+bits.swift
@@ -7,3 +7,20 @@
 //
 
 import Foundation
+
+extension UInt64 {
+    private static let kSize: UInt64 = 64
+    func rotateLeft(_ pos: UInt64) -> UInt64 {
+        let move = pos % UInt64.kSize
+        let n1 = self << move
+        let n2 = self >> (UInt64.kSize - move)
+        return n1 | n2
+    }
+
+    func rotateRight(_ pos: UInt64) -> UInt64 {
+        let move = pos % UInt64.kSize
+        let n1 = self << (UInt64.kSize - move)
+        let n2 = self >> move
+        return n1 | n2
+    }
+}
