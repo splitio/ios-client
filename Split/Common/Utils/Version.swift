39b07cb7d981e5b7acd24d5a166c57173f631eae
Javier L. Avrudsky <javier.avrudsky@split.io>
2021-09-07 11:50:12 -0300

    Release 2.11.5-rc1 - External logger for Customer to debug in prod

--- Split/Common/Utils/Version.swift
+++ Split/Common/Utils/Version.swift
@@ -9,7 +9,7 @@ import Foundation
 
 class Version {
     private static let kSdkPlatform: String = "ios"
-    private static let kVersion = "2.11.4"
+    private static let kVersion = "2.11.5-rc1"
 
     static var semantic: String {
         return kVersion
