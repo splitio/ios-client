6125b7b9e6d6a240b9651bddf467b3926bd50e57
Javier L. Avrudsky <javier.avrudsky@split.io>
2021-10-20 10:46:33 -0300

    Added static library target and bundle

---
+++ Splitlib/Bundle+Name.swift
@@ -0,0 +1,16 @@
+//
+//  Bundle+Name.swift
+//  Splitlib
+//
+//  Created by Javier Avrudsky on 19-Oct-2021.
+//  Copyright © 2021 Split. All rights reserved.
+//
+
+import Foundation
+
+extension Foundation.Bundle {
+    static var splitBundleName: String {
+        return "SplitBundle"
+    }
+}
+
---
+++ Splitlib/Splitlib.swift
@@ -0,0 +1,11 @@
+//
+//  Splitlib.swift
+//  Splitlib
+//
+//  Created by Javier Avrudsky on 18-Oct-2021.
+//  Copyright © 2021 Split. All rights reserved.
+//
+
+class Splitlib {
+
+}
