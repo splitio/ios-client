9a0a452a9df06590d806c2035605cc310884f934
Javier L. Avrudsky <javier.avrudsky@split.io>
2021-12-02 10:42:01 -0300

    Added telemetry data models

---
+++ Split/Telemetry/Storage/TelemetryConfig.swift
@@ -0,0 +1,79 @@
+//
+//  TelemetryConfig.swift
+//  Split
+//
+//  Created by Javier Avrudsky on 01-Dec-2021.
+//  Copyright © 2021 Split. All rights reserved.
+//
+
+import Foundation
+
+struct TelemetryRates {
+    var splits: Int64
+    var segments: Int64
+    var impressions: Int64
+    var events: Int64
+    var telemetry: Int64
+
+    enum CodingKeys: String, CodingKey {
+        case splits = "sp"
+        case segments = "se"
+        case impressions = "im"
+        case events = "ev"
+        case telemetry = "te"
+    }
+}
+
+struct TelemetryUrlOverrides {
+    var sdk: Bool
+    var events: Bool
+    var auth: Bool
+    var stream: Bool
+    var telemetry: Bool
+
+    enum CodingKeys: String, CodingKey {
+        case sdk = "s"
+        case events = "e"
+        case auth = "a"
+        case stream = "st"
+        case telemetry = "t"
+    }
+}
+
+struct TelemetryConfig {
+    var operationMode: Int
+    var streamingEnabled: Bool
+    var storage: String
+    var rates: TelemetryRates
+    var urlOverrides: TelemetryUrlOverrides
+    var impressionsQueueSize: Int64
+    var eventsQueueSize: Int64
+    var impressionsMode: Int
+    var impressionsListenerEnabled: Bool
+    var httpProxyDetected: Bool
+    var activeFactories: Int64
+    var redundantFactories: Int64
+    var timeUntilReady: Int64
+    var nonReadyUsages: Int64
+    var integrations: [String]?
+    var tags: [String]?
+
+    enum CodingKeys: String, CodingKey {
+        case operationMode = "oM"
+        case streamingEnabled = "sE"
+        case storage = "st"
+        case rates = "rR"
+        case urlOverrides = "uO"
+        case impressionsQueueSize = "iQ"
+        case eventsQueueSize = "eQ"
+        case impressionsMode = "iM"
+        case impressionsListenerEnabled = "iL"
+        case httpProxyDetected = "hP"
+        case activeFactories = "aF"
+        case redundantFactories = "rF"
+        case timeUntilReady = "tR"
+        case nonReadyUsages = "nR"
+        case integrations = "i"
+        case tags = "t"
+    }
+}
---
+++ Split/Telemetry/Storage/TelemetryStorage.swift
@@ -0,0 +1,39 @@
+//
+//  TelemetryStorage.swift
+//  Split
+//
+//  Created by Javier Avrudsky on 01-Dec-2021.
+//  Copyright © 2021 Split. All rights reserved.
+//
+
+import Foundation
+
+
+//
+//
+//// MARK: Config Telemtry
+//protocol TelemetryConfigProducer {
+//    func recordNonReadyUsage()
+//}
+//
+//protocol TelemetryConfigConsumer {
+//    func getNonReadyUsages()
+//}
+//
+//// MARK: Evaluation Telemetry
+//
+//struct MethodNames {
+//
+//}
+//
+//
+//
+//protocol TelemetryEvaluationProducer {
+//    func recordLatency(method: String, latency: Int64)
+//    func recordException(method: String)
+//}
+//
+//protocol TelemetryEvaluationConsumer {
+//    func recordLatency(method: String, latency: Int64)
+//    func recordException(method: String)
+//}
