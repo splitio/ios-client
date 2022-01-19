64e53f3bc16c22bc7bc741ba5921bc7b941b82bc
Javier L. Avrudsky <javier.avrudsky@split.io>
2022-01-19 19:32:05 -0300

    Release 2.12.2-rc3

--- Split/Common/Utils/Version.swift
+++ Split/Common/Utils/Version.swift
@@ -9,7 +9,7 @@ import Foundation
 
 class Version {
     private static let kSdkPlatform: String = "ios"
-    private static let kVersion = "2.12.2-rc2"
+    private static let kVersion = "2.12.2-rc3"
 
     static var semantic: String {
         return kVersion
