3426c58a16da1767b8dcd323a93b064ab8138a9e
runner <runner@Mac-1643726908679.local>
2022-02-01 14:52:36 +0000

    Added new release xcframework url and checksum

--- Package.swift
+++ Package.swift
@@ -27,7 +27,8 @@ let package = Package(
         ),
         .binaryTarget(
             name: "SplitXcFramework",
-            url: "https://aws-development-split-public.s3.amazonaws.com/mobile/ios-xcframework/Split_1.0.0.zip"
+            url: "https://aws-development-split-public.s3.amazonaws.com/mobile/ios-xcframework/Split_1.0.0.zip",
+            checksum: "39c9e0f11301572e1e027397e8668038526300b9531ee9649473f12c97006be0"
         ),
         .target(
             name: "JFBCrypt",
