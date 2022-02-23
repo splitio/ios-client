51ae34955d41d31b02683fb04f29ec19f615121d
Javier Avrudsky <javier.avrudsky@split.io>
2022-02-23 16:45:25 -0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -28,7 +28,7 @@ let package = Package(
         .binaryTarget(
             name: "SplitXcFramework",
             url: "https://aws-development-split-public.s3.amazonaws.com/mobile/ios-xcframework/split-ios-2.13.1-rc3.zip",
-            checksum: "8266d7f6cdb3ab22ba49a4e6b0e90d03381d3003540d71d01f6a6e8b84eecad0"
+            checksum: "8266d7f6cdb3ab22ba49a4e6b0e90d03381d3003540d71d01f6a6e8b84eecad0_____"
         ),
         .target(
             name: "JFBCrypt",
