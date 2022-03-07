f5ceb8b6c04cc5bd5afd041c2f5f954b6762f71c
Javier L. Avrudsky <javier.avrudsky@split.io>
2022-03-07 12:02:46 -0300

    Reverting package.swift to remove xcframework buildings placeholders

--- Package.swift
+++ Package.swift
@@ -6,9 +6,7 @@ let package = Package(
     name: "Split",
     platforms: [.iOS(.v9)],
     products: [
-        .library(
-            name: "Split", 
-            targets: ["Split", "SplitXcFramework"])
+        .library(name: "Split", targets: ["Split"])
     ],
     dependencies: [
         .package(name: "Swifter", url: "https://github.com/httpswift/swifter.git", from: "1.5.0")
@@ -22,14 +20,8 @@ let package = Package(
                 "Common/Utils/JFBCrypt/",
                 "Common/Yaml/LICENSE",
                 "Info.plist",
-                "Split.h",
-                "Common/Utils/framework/HashHelper.swift"
+                "Split.h"
             ]
-        ),
-        .binaryTarget(
-            name: "SplitXcFramework",
-            url: "https://split-public.s3.amazonaws.com/sdk/split-ios-2.13.1.zip",
-            checksum: "5c8fd88b169ac72643684d3fd461662c70d1ee2b58b3e55f874b880ab0160c7f"
         ),
         .target(
             name: "JFBCrypt",
