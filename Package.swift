528619c64d478a50a84e9d2b4c80dd30703034b4
Javier L. Avrudsky <javier.avrudsky@split.io>
2022-09-08 12:41:25 -0300

    Adding macos to manifest

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 
 let package = Package(
     name: "Split",
-    platforms: [.iOS(.v9)],
+    platforms: [.iOS(.v9), .macOS(.v10_15)],
     products: [
         .library(name: "Split", targets: ["Split"])
     ],
