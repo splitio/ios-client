bcac7d8da164bad6f6bbc9f9530e875fcb246483
Jeff Meadows <jmeadows@box.com>
2022-12-01 14:46:23 -0800

    Make package extension safe

--- Split/FetcherEngine/Refresh/SplitBgSynchronizer.swift
+++ Split/FetcherEngine/Refresh/SplitBgSynchronizer.swift
@@ -55,6 +55,7 @@ import BackgroundTasks
         globalStorage.remove(item: .backgroundSyncSchedule)
     }
 
+    @available(iOSApplicationExtension, unavailable)
     @objc public func schedule(serviceEndpoints: ServiceEndpoints? = nil) {
         if #available(iOS 13.0, tvOS 13.0, macCatalyst 13.1, *) {
             let success = BGTaskScheduler.shared.register(
