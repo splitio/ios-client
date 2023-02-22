fc99dc29b922d806a2cc734e86193a8ef3b2b031
Javier L. Avrudsky <javier.avrudsky@split.io>
2023-02-22 17:32:18 -0300

    New target to build extensions

--- Package.swift
+++ Package.swift
@@ -7,12 +7,24 @@ let package = Package(
     platforms: [.iOS(.v9), .macOS(.v10_11), .watchOS(.v7), .tvOS(.v9)],
     products: [
         .library(name: "Split", targets: ["Split"])
+        .library(name: "SplitExt", targets: ["SplitExt"])
     ],
     targets: [
         .target(
             name: "Split",
             path: "Split",
             exclude: [
+                "Common/Yaml/LICENSE",
+                "Info.plist",
+                "Split.h"
+            ]
+        ),
+        .target(
+            name: "SplitExt",
+            path: "Split",
+            exclude: [
+                "FetcherEngine/Refresh/BackgroundSyncWorker.swift",
+                "FetcherEngine/Refresh/SplitBgSynchronizer.swift",
                 "Common/Yaml/LICENSE",
                 "Info.plist",
                 "Split.h"
