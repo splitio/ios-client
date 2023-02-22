648f026a74d901acef61f21518e90b99ef7c496b
Javier L. Avrudsky <javier.avrudsky@split.io>
2023-02-22 18:39:00 -0300

    Renaming target for extension

--- Package.swift
+++ Package.swift
@@ -7,7 +7,7 @@ let package = Package(
     platforms: [.iOS(.v9), .macOS(.v10_11), .watchOS(.v7), .tvOS(.v9)],
     products: [
         .library(name: "Split", targets: ["Split", "BgSync"]),
-        .library(name: "SplitExt", targets: ["Split"])
+        .library(name: "SplitForExtension", targets: ["Split"])
     ],
     targets: [
         .target(
