a039f82460dd83c0a0459125722215e177113403
Javier L. Avrudsky <javier.avrudsky@split.io>
2023-02-22 17:51:18 -0300

    Creating a new target for by sync

--- Package.swift
+++ Package.swift
@@ -6,28 +6,23 @@ let package = Package(
     name: "Split",
     platforms: [.iOS(.v9), .macOS(.v10_11), .watchOS(.v7), .tvOS(.v9)],
     products: [
-        .library(name: "Split", targets: ["Split"]),
-        .library(name: "SplitExt", targets: ["SplitExt"])
+        .library(name: "Split", targets: ["Split", "BgSync"]),
+        .library(name: "SplitExt", targets: ["Split"])
     ],
     targets: [
         .target(
             name: "Split",
             path: "Split",
             exclude: [
+                "BgSync",
                 "Common/Yaml/LICENSE",
                 "Info.plist",
                 "Split.h"
             ]
         ),
         .target(
-            name: "SplitExt",
-            path: "Split",
-            exclude: [
-                "FetcherEngine/Refresh/BackgroundSyncWorker.swift",
-                "FetcherEngine/Refresh/SplitBgSynchronizer.swift",
-                "Common/Yaml/LICENSE",
-                "Info.plist",
-                "Split.h"
+            name: "BgSync",
+            path: "Split/BgSync"
             ]
         )
     ]
