2053b0ff393db2da81241b768db57f0122fd1ee3
Javier L. Avrudsky <javier.avrudsky@split.io>
2023-06-22 11:40:05 -0300

    Increased time to try in GA

--- SplitTests/Streaming/PushNotificationManagerTest.swift
+++ SplitTests/Streaming/PushNotificationManagerTest.swift
@@ -215,7 +215,7 @@ class PushNotificationManagerTest: XCTestCase {
         timersManager.reset()
         pnManager.reset()
 
-        wait(for: [exp], timeout: 5)
+        wait(for: [exp], timeout: 10)
 
         let streamEvents = telemetryProducer.streamingEvents
 
