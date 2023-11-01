de4a80f5e830800bc2d726855f8e28ef71ff786e
Javier L. Avrudsky <javier.avrudsky@split.io>
2023-11-01 12:01:21 -0300

    Release 2.23.0 - Renamed from 2.22.1

--- Split/Common/Utils/Version.swift
+++ Split/Common/Utils/Version.swift
@@ -9,7 +9,7 @@ import Foundation
 
 class Version {
     private static let kSdkPlatform: String = "ios"
-    private static let kVersion = "2.22.1"
+    private static let kVersion = "2.23.0"
 
     static var semantic: String {
         return kVersion
