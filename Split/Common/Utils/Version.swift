2b929c9b2f6ba63b1654b54e60d5b921b2fcadcf
Gaston Thea <gaston.thea@split.io>
2024-02-05 17:52:49 -0300

    Version 2.24.3

--- Split/Common/Utils/Version.swift
+++ Split/Common/Utils/Version.swift
@@ -9,7 +9,7 @@ import Foundation
 
 class Version {
     private static let kSdkPlatform: String = "ios"
-    private static let kVersion = "2.24.3-rc1"
+    private static let kVersion = "2.24.3"
 
     static var semantic: String {
         return kVersion
