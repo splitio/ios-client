976d9eeec1cdc1512b6185e7f31eab1673357822
Javier L. Avrudsky <javier.avrudsky@split.io>
2024-03-07 13:28:55 -0300

    Removing unused class

--- Split/Events/SplitEventTask.swift
+++ Split/Events/SplitEventTask.swift
@@ -7,17 +7,6 @@
 
 import Foundation
 
-//open class SplitEventTask: NSObject {
-//    public var event: SplitEvent?
-//    @objc open func onPostExecute(client: SplitClient) {
-//        debugPrint("* running superclass.onPostExecute. This method should be override by developers")
-//    }
-//
-//    @objc open func onPostExecuteView(client: SplitClient) {
-//        debugPrint("* running superclass.onPostExecuteView. This method should be override by developers")
-//    }
-//}
-
 protocol SplitEventTask {
     var event: SplitEvent { get }
     var runInBackground: Bool { get }
