7dc0acd10a00bb5ce781c2f2a75adb8cf9e1596c
Javier L. Avrudsky <javier.avrudsky@split.io>
2024-05-06 10:12:09 -0300

    Release 2.24.7-rc1

--- Split/Common/Utils/Version.swift
+++ Split/Common/Utils/Version.swift
@@ -9,7 +9,7 @@ import Foundation
 
 class Version {
     private static let kSdkPlatform: String = "ios"
-    private static let kVersion = "2.24.6"
+    private static let kVersion = "2.24.7-rc1"
 
     static var semantic: String {
         return kVersion
