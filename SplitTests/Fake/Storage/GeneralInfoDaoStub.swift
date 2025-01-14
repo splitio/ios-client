ebb993b7357768d10b04b42e77a4c14cb59ddb58
Gaston Thea <gaston.thea@harness.io>
2025-01-14 16:48:00 -0300

    General info storage

--- SplitTests/Fake/Storage/GeneralInfoDaoStub.swift
+++ SplitTests/Fake/Storage/GeneralInfoDaoStub.swift
@@ -1,11 +1,3 @@
-//
-//  GeneralInfoDaoStub.swift
-//  SplitTests
-//
-//  Created by Javier L. Avrudsky on 18/11/2020.
-//  Copyright © 2020 Split. All rights reserved.
-//
-
 @testable import Split
 import Foundation
 
@@ -17,15 +9,15 @@ class GeneralInfoDaoStub: GeneralInfoDao {
     func update(info: GeneralInfo, stringValue: String) {
         updatedString[info.rawValue] = stringValue
     }
-    
+
     func update(info: GeneralInfo, longValue: Int64) {
         updatedLong[info.rawValue] = longValue
     }
-    
+
     func stringValue(info: GeneralInfo) -> String? {
         return updatedString[info.rawValue]
     }
-    
+
     func longValue(info: GeneralInfo) -> Int64? {
         return updatedLong[info.rawValue]
     }
