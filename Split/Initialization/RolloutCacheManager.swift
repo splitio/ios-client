a604b22b8efad5336c34d31f39b5d23f695fcc3a
gthea <gaston.thea@harness.io>
2025-01-20 19:18:24 -0300

    RolloutCacheManager implementation (#606)

--- Split/Initialization/RolloutCacheManager.swift
+++ Split/Initialization/RolloutCacheManager.swift
@@ -1,24 +1,62 @@
 import Foundation
 
 protocol RolloutCacheManager {
-    func validateCache(listener: (() -> Void)?)
+    func validateCache(listener: (() -> Void))
 }
 
 class DefaultRolloutCacheManager: RolloutCacheManager {
     private let kMinCacheClearDays = 1
     private let generalInfoStorage: GeneralInfoStorage
-    private let splitsStorage: SplitsStorage
-    private let mySegmentsStorage: MySegmentsStorage
-    private let myLargeSegmentsStorage: MySegmentsStorage
-    
-    init(generalInfoStorage: GeneralInfoStorage, splitsStorage: SplitsStorage, mySegmentsStorage: MySegmentsStorage, myLargeSegmentsStorage: MySegmentsStorage) {
+    private let rolloutCacheConfiguration: RolloutCacheConfiguration
+    private let rolloutDefinitionsCache: [RolloutDefinitionsCache]
+
+    init(generalInfoStorage: GeneralInfoStorage,
+         rolloutCacheConfiguration: RolloutCacheConfiguration,
+         storages: RolloutDefinitionsCache...) {
         self.generalInfoStorage = generalInfoStorage
-        self.splitsStorage = splitsStorage
-        self.mySegmentsStorage = mySegmentsStorage
-        self.myLargeSegmentsStorage = myLargeSegmentsStorage
+        self.rolloutCacheConfiguration = rolloutCacheConfiguration
+        self.rolloutDefinitionsCache = storages
     }
-    
-    func validateCache(listener: (() -> Void)?) {
-        // TODO: implementation
+
+    func validateCache(listener: (() -> Void)) {
+        defer {
+            listener()
+        }
+
+        if shouldClear() {
+            clear()
+        }
+    }
+
+    private func shouldClear() -> Bool {
+        let lastUpdateTimestamp = generalInfoStorage.getUpdateTimestamp()
+        let daysSinceLastUpdate = Date.secondsToDays(seconds: Date.now() - lastUpdateTimestamp)
+
+        if lastUpdateTimestamp > 0 && daysSinceLastUpdate >= rolloutCacheConfiguration.expirationDays {
+            Logger.v("Clearing rollout definitions cache due to expiration")
+            return true
+        } else if rolloutCacheConfiguration.clearOninit {
+            let lastCacheClearTimestamp = generalInfoStorage.getRolloutCacheLastClearTimestamp()
+            if lastCacheClearTimestamp < 1 { // 0 is default value for rollout cache timestamp
+                return true
+            }
+            let daysSinceCacheClear = Date.secondsToDays(seconds: Date.now() - lastCacheClearTimestamp)
+
+            // don't clear too soon
+            if daysSinceCacheClear >= kMinCacheClearDays {
+                Logger.v("Forcing rollout definitions cache clear")
+                return true
+            }
+        }
+
+        return false
+    }
+
+    private func clear() {
+        for cache in rolloutDefinitionsCache {
+            cache.clear()
+        }
+        generalInfoStorage.setRolloutCacheLastClearTimestamp(timestamp: Date.now())
+        Logger.v("Rollout definitions cache cleared")
     }
 }
