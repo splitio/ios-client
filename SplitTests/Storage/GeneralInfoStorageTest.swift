c923c35c6bee1761c7518d6fffbffa1b86988725
Gaston Thea <gaston.thea@harness.io>
2025-01-24 15:37:13 -0300

    Add missing GeneralInfoStorage tests

--- SplitTests/Storage/GeneralInfoStorageTest.swift
+++ SplitTests/Storage/GeneralInfoStorageTest.swift
@@ -44,4 +44,28 @@ final class GeneralInfoStorageTest: XCTestCase {
     func testGetRolloutCacheLastClearTimestampReturnsZeroIfEntityIsNil() throws {
         XCTAssertEqual(generalInfoStorage.getRolloutCacheLastClearTimestamp(), 0)
     }
+
+    func testSetSplitsFilterQueryStringSetsValueOnDao() throws {
+        generalInfoStorage.setSplitsFilterQueryString(filterQueryString: "query")
+
+        XCTAssertEqual(generalInfoDao.updatedString, ["splitsFilterQueryString": "query"])
+    }
+
+    func testGetSplitsFilterQueryStringGetsValueFromDao() throws {
+        generalInfoDao.update(info: .splitsFilterQueryString, stringValue: "query")
+
+        XCTAssertEqual(generalInfoStorage.getSplitsFilterQueryString(), "query")
+    }
+
+    func testSetFlagsSpecSetsValueOnDao() throws {
+        generalInfoStorage.setFlagSpec(flagsSpec: "2.2")
+
+        XCTAssertEqual(generalInfoDao.updatedString, ["flagsSpec": "2.2"])
+    }
+
+    func testGetFlagsSpecGetsValueFromDao() throws {
+        generalInfoDao.update(info: .flagsSpec, stringValue: "2.1")
+
+        XCTAssertEqual(generalInfoStorage.getFlagSpec(), "2.1")
+    }
 }
