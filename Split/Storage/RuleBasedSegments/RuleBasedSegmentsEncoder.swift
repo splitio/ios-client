31558f9c10b387ebe3c832582bf8e1e1d01689a6
Gaston Thea <gaston.thea@harness.io>
2025-03-17 17:14:46 -0300

    RBS DAO

---
+++ Split/Storage/RuleBasedSegments/RuleBasedSegmentsEncoder.swift
@@ -0,0 +1,42 @@
+//
+//  RuleBasedSegmentsEncoder.swift
+//  Split
+//
+//  Created by Split on 14/03/2025.
+//  Copyright © 2025 Split. All rights reserved.
+//
+
+import Foundation
+
+protocol RuleBasedSegmentsEncoder {
+    func encode(_ segments: [RuleBasedSegment]) -> [String: String]
+}
+
+class RuleBasedSegmentsSerialEncoder: RuleBasedSegmentsEncoder {
+
+    private var cipher: Cipher?
+
+    init(cipher: Cipher? = nil) {
+        self.cipher = cipher
+    }
+
+    func encode(_ segments: [RuleBasedSegment]) -> [String: String] {
+        var result = [String: String]()
+
+        for segment in segments {
+            guard let segmentName = segment.name?.lowercased() else {
+                continue
+            }
+
+            do {
+                let json = try Json.encodeToJson(segment)
+                let encryptedName = cipher?.encrypt(segmentName) ?? segmentName
+                let encryptedJson = cipher?.encrypt(json) ?? json
+                result[encryptedName] = encryptedJson
+            } catch {
+                Logger.e("Error while encoding rule based segment: \(error.localizedDescription)")
+            }
+        }
+        return result
+    }
+}
