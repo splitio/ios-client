ef582d33f4cf40c0700c4d8b44abc4ef7dc8deeb
Martin Cardozo <martin.cardozo@harness.io>
2025-05-23 10:42:25 -0300

    Split tests

--- SplitTests/SplitDTOTests.swift
+++ SplitTests/SplitDTOTests.swift
@@ -6,30 +6,31 @@ import XCTest
 
 class SplitDTOTests: XCTestCase {
     
-    let expectedFlags = ["flag1","flag2"]
-    let expectedTreatments = [["on","v1"],["off"]]
-    
-    func testPrerequisitesField() {
+    func testCorrectPrerequisites() {
         
-        // Happy case
-        var data = correctJson
-        var decoded = try? TargetingRulesChangeDecoder.decode(from: data.data(using: .utf8)!)
+        let expectedFlags = ["flag1","flag2"]
+        let expectedTreatments = [["on","v1"],["off"]]
         
+        let data = correctJson
+        
+        let decoded = try? TargetingRulesChangeDecoder.decode(from: data.data(using: .utf8)!)
         for i in 0..<decoded!.featureFlags.splits[0].prerequisites!.count {
             let prerequisite = decoded!.featureFlags.splits[0].prerequisites![i]
-            
             XCTAssertEqual(prerequisite.n!, expectedFlags[i], "Prerequisites flag names should be the same")
             XCTAssertEqual(prerequisite.ts!.joined(separator: ","), expectedTreatments[i].joined(separator: ","), "Prerequisites treatments should be the same")
         }
+    }
+    
+    func testEmptyPrerequisites() {
+        let data = jsonWithEmptyPrerequisites
         
-        // Empty prerequisites
-        data = jsonWithEmptyPrerequisites
-        decoded = try? TargetingRulesChangeDecoder.decode(from: data.data(using: .utf8)!)
+        let decoded = try? TargetingRulesChangeDecoder.decode(from: data.data(using: .utf8)!)
         XCTAssertEqual(decoded?.featureFlags.splits.first?.prerequisites?.first?.n, nil, "Prerequisites field should be empty")
         XCTAssertEqual(decoded?.ruleBasedSegments.segments.first?.name, "test_segment", "Rest of the response should be correct")
-        
-        // Error in JSON
-        data = jsonWithMalformedPrerequisites
+    }
+    
+    func testMalformedPrerequisites() {
+        let data = jsonWithMalformedPrerequisites
         XCTAssertThrowsError(try TargetingRulesChangeDecoder.decode(from: data.data(using: .utf8)!), "Wrong JSON should throw decoding error")
     }
     
