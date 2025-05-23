57a5bf8479112c3d0dc42c8399337cfedf5a1304
Martin Cardozo <martin.cardozo@harness.io>
2025-05-23 10:58:02 -0300

    New Matcher

---
+++ Split/Matchers/PrerequisitesMatcher.swift
@@ -0,0 +1,14 @@
+//  Created by Martin Cardozo on 22/05/2025.
+//  Copyright © 2025 Split. All rights reserved.
+
+import Foundation
+
+class PrerequisitesMatcher: BaseMatcher, MatcherProtocol {
+
+    private var prerequisites: [Prerequisite]?
+    //private var storage: Storage?
+
+    func evaluate(values: EvalValues, context: EvalContext?) -> Bool {
+
+    }
+}
