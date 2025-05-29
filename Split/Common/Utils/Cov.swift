ae9eee00ab1ea07c202dc995723a15da9c73fecc
Gaston Thea <gaston.thea@harness.io>
2025-05-29 11:53:33 -0300

    Test cov file

---
+++ Split/Common/Utils/Cov.swift
@@ -0,0 +1,13 @@
+//
+//  Cov.swift
+//  Split
+//
+//  Copyright © 2025 Split. All rights reserved.
+//
+
+class Cov {
+
+    static func isItTrue(input: Int) -> Bool {
+        return input % 2 == 0
+    }
+}
