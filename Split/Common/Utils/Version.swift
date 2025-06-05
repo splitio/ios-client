c0ed4ecb3d3459189b8e97a595a57b81627f6548
Gaston Thea <gaston.thea@harness.io>
2025-06-05 14:25:09 -0300

    chore: Update version to 3.3.1-rc1

--- Split/Common/Utils/Version.swift
+++ Split/Common/Utils/Version.swift
@@ -9,7 +9,7 @@ import Foundation
 
 class Version {
     private static let kSdkPlatform: String = "ios"
-    private static let kVersion = "3.3.1-rc0"
+    private static let kVersion = "3.3.1-rc1"
 
     static var semantic: String {
         return kVersion
