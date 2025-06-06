bdd5b9a9aa25d641a5b40441bbc23d2c3bff3e8d
Martin Cardozo <martin.cardozo@harness.io>
2025-06-06 15:22:13 -0300

    Simplified logic

--- Split/Events/MetadataType.swift
+++ Split/Events/MetadataType.swift
@@ -7,18 +7,7 @@
     case LARGE_SEGMENT_UPDATED
     case RULE_BASED_SEGMENT_UPDATED
     
-    public func toString() -> String {
-        switch self {
-            case .FLAG_UPDATED:
-                return "FLAG_UPDATED"
-            case .FLAG_KILLED:
-                return "FLAG_KILLED"
-            case .SEGMENT_UPDATED:
-                return "SEGMENT_UPDATED"
-            case .LARGE_SEGMENT_UPDATED:
-                return "LARGE_SEGMENT_UPDATED"
-            case .RULE_BASED_SEGMENT_UPDATED:
-                return "RULE_BASED_SEGMENT_UPDATED"
-        }
+    var stringValue: String {
+        return String(describing: self)
     }
 }
