79fc3c296e72a806b97eb15dd392a29be65e6fd2
Martin Cardozo <martin.cardozo@harness.io>
2025-08-26 10:41:25 -0300

    Data entities added

---
+++ Split/Engine/FallbackTreatments/FallbackTreatments.swift
@@ -0,0 +1,25 @@
+//  Created by Martin Cardozo on 25/08/2025
+
+public struct FallbackTreatment: Equatable, Hashable {
+    
+    let treatment: String
+    let config: String?
+    let label: String
+    
+    init(_ treatment: String, config: String? = nil) {
+        self.treatment = treatment
+        self.config = config
+        self.label = "fallback treatment" // Constant alongisde the other impression labels (e.g.:  "fallback treatment - CONTROL" )
+    }
+}
+
+public struct FallbackConfig: Equatable, Hashable {
+    
+    let global: FallbackTreatment? // Default treatment for all
+    let byFlag: [String: FallbackTreatment] // Fallback treatment per flag
+    
+    init(global: FallbackTreatment? = nil, byFlag: [String: FallbackTreatment] = [:]) {
+        self.global = global
+        self.byFlag = byFlag
+    }
+}
