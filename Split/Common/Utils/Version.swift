e5a1f6e7c65252d7f742ebb8ca013361a27700d8
Martin Cardozo <martin.cardozo@harness.io>
2025-08-29 12:09:56 -0300

    chore: Update version to 3.3.3 and update CHANGES.txt

--- Split/Common/Utils/Version.swift
+++ Split/Common/Utils/Version.swift
@@ -9,7 +9,7 @@ import Foundation
 
 class Version {
     private static let kSdkPlatform: String = "ios"
-    private static let kVersion = "3.4.0-rc4"
+    private static let kVersion = "3.3.3"
 
     static var semantic: String {
         return kVersion
