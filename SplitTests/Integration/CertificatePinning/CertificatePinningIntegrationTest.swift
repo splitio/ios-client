1869778c3c5cb3fd475e4449d6bcd3ce14de9a45
Gaston Thea <gaston.thea@harness.io>
2025-12-09 20:33:50 -0300

    Integration test

---
+++ SplitTests/Integration/CertificatePinning/CertificatePinningIntegrationTest.swift
@@ -0,0 +1,60 @@
+//
+//  CertificatePinningIntegrationTest.swift
+//  SplitTests
+//
+
+import XCTest
+@testable import Split
+
+class CertificatePinningIntegrationTest: XCTestCase {
+    
+    var factory: SplitFactory?
+    
+    override func tearDown() {
+        HttpSessionConfig.default.pinChecker = nil
+        HttpSessionConfig.default.notificationHelper = nil
+        super.tearDown()
+    }
+
+    func testFactoryWiringOfStatusHandler() {
+        let expectation = self.expectation(description: "Status handler called")
+        let testHost = "www.test.com"
+        let testReason = "TestReason"
+        let testStatus = CertificatePinningStatus.success
+        
+        let pinningConfigBuilder = CertificatePinningConfig.builder()
+        pinningConfigBuilder.addPin(host: "www.google.com", keyHash: "sha256/AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=")
+        
+        pinningConfigBuilder.statusHandler { host, status, reason in
+            XCTAssertEqual(host, testHost)
+            XCTAssertEqual(status, testStatus)
+            XCTAssertEqual(reason, testReason)
+            expectation.fulfill()
+        }
+        
+        guard let pinningConfig = try? pinningConfigBuilder.build() else {
+            XCTFail("Failed to build pinning config")
+            return
+        }
+
+        let builder = IntegrationHelper().simplestFactoryWithDummyKeys()
+        let splitConfig = SplitClientConfig()
+        splitConfig.certificatePinningConfig = pinningConfig
+        _ = builder.setConfig(splitConfig)
+
+        factory = builder.build()
+        XCTAssertNotNil(factory)
+        XCTAssertNotNil(HttpSessionConfig.default.pinChecker)
+        XCTAssertNotNil(HttpSessionConfig.default.notificationHelper)
+        
+        // Simulate a notification to prove the listener is active
+        guard let notificationHelper = HttpSessionConfig.default.notificationHelper else {
+            XCTFail("Notification helper is nil")
+            return
+        }
+        let statusObj = CertificatePinningCompleteStatus(host: testHost, status: testStatus, reason: testReason)
+        notificationHelper.post(notification: .pinnedCredentialStatus, info: statusObj)
+        
+        waitForExpectations(timeout: 1.0, handler: nil)
+    }
+}
