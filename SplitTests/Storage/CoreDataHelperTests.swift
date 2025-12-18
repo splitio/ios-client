57cce4351b2e67b2501a1cd5884115a28d5b007c
Gaston Thea <gaston.thea@harness.io>
2025-12-18 12:36:47 -0300

    CoreDataHelper test

---
+++ SplitTests/Storage/CoreDataHelperTests.swift
@@ -0,0 +1,69 @@
+//
+//  CoreDataHelperTests.swift
+//  SplitTests
+//
+
+import Foundation
+import XCTest
+import CoreData
+@testable import Split
+
+class CoreDataHelperTests: XCTestCase {
+    
+    var coreDataHelper: CoreDataHelper!
+    
+    override func setUp() {
+        let queue = DispatchQueue(label: "coredata helper test")
+        coreDataHelper = IntegrationCoreDataHelper.get(databaseName: "test", dispatchQueue: queue)
+    }
+    
+    func testSaveWithErrorHandlingSucceedsWhenChangesExist() {
+        coreDataHelper.performAndWait {
+            _ = self.coreDataHelper.create(entity: .generalInfo)
+        }
+        
+        XCTAssertNoThrow(try coreDataHelper.saveWithErrorHandling())
+    }
+    
+    func testSaveWithErrorHandlingSucceedsWhenNoChanges() {
+        XCTAssertNoThrow(try coreDataHelper.saveWithErrorHandling())
+    }
+    
+    func testSaveWithErrorHandlingPersistsData() {
+        coreDataHelper.performAndWait {
+            if let entity = self.coreDataHelper.create(entity: .generalInfo) as? GeneralInfoEntity {
+                entity.name = GeneralInfo.splitsChangeNumber.rawValue
+                entity.longValue = 12345
+            }
+        }
+        
+        try? coreDataHelper.saveWithErrorHandling()
+        
+        let fetched = coreDataHelper.fetch(entity: .generalInfo)
+        XCTAssertEqual(1, fetched.count)
+        
+        if let entity = fetched.first as? GeneralInfoEntity {
+            XCTAssertEqual(12345, entity.longValue)
+        } else {
+            XCTFail("Expected GeneralInfoEntity")
+        }
+    }
+    
+    func testSaveWithErrorHandlingThrowsOnInvalidContext() {
+        let invalidContext = NSManagedObjectContext(concurrencyType: .privateQueueConcurrencyType)
+        let invalidHelper = CoreDataHelper(
+            managedObjectContext: invalidContext,
+            persistentCoordinator: NSPersistentStoreCoordinator(managedObjectModel: NSManagedObjectModel())
+        )
+        
+        invalidContext.performAndWait {
+            let entity = NSEntityDescription()
+            entity.name = "TestEntity"
+            entity.managedObjectClassName = NSStringFromClass(NSManagedObject.self)
+        }
+        
+        // Context without persistent store should not throw when there are no changes
+        XCTAssertNoThrow(try invalidHelper.saveWithErrorHandling())
+    }
+}
+
