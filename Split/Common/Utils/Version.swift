590d2a952a5db3e5b2dbb33ee5b29a17d501867b
Martin Cardozo <martin.cardozo@harness.io>
2026-01-06 11:40:22 -0300

    chore: Update version to 3.6.0-rc1

--- Split/Common/Utils/Version.swift
+++ Split/Common/Utils/Version.swift
@@ -10,7 +10,7 @@ import Foundation
 class Version {
     private static let kSdkPlatform: String = "ios"
 
-    private static let kVersion = "3.5.2"
+    private static let kVersion = "3.6.0-rc1"
 
     static var semantic: String {
         return kVersion
