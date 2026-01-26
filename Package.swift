57f07911dfc00a853046c35ba5ba20509b227a2c
Gaston Thea <gaston.thea@harness.io>
2026-01-26 11:56:24 -0300

    WIP kaban

--- Package.swift
+++ Package.swift
@@ -7,16 +7,30 @@ let package = Package(
     platforms: [.iOS(.v9), .macOS(.v10_11), .watchOS(.v7), .tvOS(.v9)],
     products: [
         .library(name: "Split", targets: ["Split"])
-    ],
+    
+        .library(name: "SplitCommons", targets: ["Logging"]),],
     targets: [
         .target(
             name: "Split",
-            path: "Split",
+                        dependencies: ["Logging"],
+path: "Split",
             exclude: [
                 "Common/Yaml/LICENSE",
                 "Info.plist",
                 "Split.h"
             ]
-        )
+        ),
+    
+        .target(
+            name: "Logging",
+            dependencies: [],
+            exclude: ["Tests", "README.md"]
+        ),
+        .testTarget(
+            name: "LoggingTests",
+            dependencies: ["Logging"],
+            path: "Sources/Logging/Tests"
+        ),
+        // #INJECT_TARGET
     ]
 )
---
+++ Sources/Logging/Tests/LoggingTests.swift
@@ -0,0 +1,9 @@
+import XCTest
+@testable import Logging
+
+final class LoggingTests: XCTestCase {
+    func testExample() {
+        // Add your tests here
+        XCTAssertTrue(true)
+    }
+}
