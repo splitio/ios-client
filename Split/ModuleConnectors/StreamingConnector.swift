7dc42625b26116de89f15c30356a81f711c561a6
Gaston Thea <gaston.thea@harness.io>
2026-04-07 18:07:41 -0300

    streaming-extract: Integrate Streaming module into Split
    - Wire Streaming module into Split and SplitWatchOS via StreamingConnector typealiases
    - Fix watchOS build: add watchsimulator/watchos to SUPPORTED_PLATFORMS for Concurrency and Streaming Xcode targets

---
+++ Split/ModuleConnectors/StreamingConnector.swift
@@ -0,0 +1,16 @@
+//  Streaming module imports for the Split module
+
+#if !COCOAPODS
+
+import Streaming
+
+// MARK: - Internal use
+typealias EventStreamParser = Streaming.EventStreamParser
+typealias SseClient = Streaming.SseClient
+typealias DefaultSseClient = Streaming.DefaultSseClient
+typealias SseClientConstants = Streaming.SseClientConstants
+typealias SseClientFactory = Streaming.SseClientFactory
+typealias DefaultSseClientFactory = Streaming.DefaultSseClientFactory
+typealias SseConnectionHandler = Streaming.SseConnectionHandler
+typealias SseHandler = Streaming.SseHandler
+#endif
